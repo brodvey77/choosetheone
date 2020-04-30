c95627c9dfcef3700a02b87c199b067a5a5ed6a0
Serj <asderd@list.ru>
2020-04-30 08:59:18 +0300

    4-th done

--- ДЗ 3/__init__.py
+++ ДЗ 3/__init__.py
@@ -1,2 +1,79 @@
 # -*- coding: utf-8 -*-
 
+# print('I will be a Python developer')
+#
+# print(3 + 7)
+# print(4 ** 3)
+# print(29 % 5)
+#
+# car_model = 'RENAULT'
+# car_color = 'RED'
+# car_year = 2013
+# car_door = 4
+#
+# car_model = 'Nissan'
+# car_color = 'yellow'
+# car_year = 1999
+# car_door = 5
+#
+# print(car_model, car_color, car_year, car_door)
+
+# jack_year_old = 15
+# jaison_year_old = 98
+# anna_year_old = 18
+# lena_year_old = 23
+# one_year = 4
+#
+# average_age = ((jack_year_old + jaison_year_old + anna_year_old + lena_year_old) / one_year)
+# print(average_age)
+
+# name = 'Jack'
+# surname = 'Nilson'
+# age = 34
+#
+# print('Hi! My name is', name, surname, 'i\'m', age, 'years old')
+
+# print('''Baa, baa, black sheep,
+#
+#          Have you any wool?
+#
+#         Yes sir, yes sir,
+#
+#         Three bags full
+#
+#
+#         One for the master,
+#
+#         One for the dame,
+#
+#         And one for the little boy
+#
+#         Who lives down the lane
+#
+#
+#         Baa, baa, black sheep,
+#
+#         Have you any wool?
+#
+#         Yes sir, yes sir,
+#
+#         Three bags full''')
+
+#
+# print('asasasasasasasasasasasasas'
+#      '\n asasasasasasa asasasasas')
+
+
+greeting = 'Hello Python!'
+print(greeting[3])
+
+print('Hello Python!'[3])
+print(greeting[0:2])
+print(greeting[:2])
+print(greeting[-13:-11])
+print(greeting[:2:1])
+print(greeting[-13:-11:1])
+
+
+
+print(greeting[6] + 'a' + greeting[-5:-3])
