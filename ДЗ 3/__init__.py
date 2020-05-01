db12f5725f0c1c5c25d63a127f4c7c037b56f4dc
Sergey <s264157250r@gmail.com>
2020-05-01 20:45:17 +0300

    add ifelifelse

--- ДЗ 3/__init__.py
+++ ДЗ 3/__init__.py
@@ -145,13 +145,28 @@
 # print(x != y)
 
 
-x, y = 'A', 'a'
-print('A' > 'a')
-print('A' < 'a')
-print('A' >= 'a')
-print('A' <= 'a')
-print('A' == 'a')
-print('A' != 'a')
+# x, y = 'A', 'a'
+# print('A' > 'a')
+# print('A' < 'a')
+# print('A' >= 'a')
+# print('A' <= 'a')
+# print('A' == 'a')
+# print('A' != 'a')
+#
+# print(ord('A'))
+# print(ord('a'))
 
-print(ord('A'))
-print(ord('a'))
+# if
+
+# x = input('Enter any number')
+# if x == '7':
+#     print('7 is a lucky number! Today is your lucky day!')
+# else:
+#     print('Thank you! Have a nice day!')
+
+# user_input = input('Enter an integer number')
+#
+# if int(user_input) % 2 == 0:
+#     print('The number is even')
+# else:
+#     print('The number is odd')
