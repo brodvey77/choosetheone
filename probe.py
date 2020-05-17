362d126746b9ea8220402e7d7b1a9466b84e5a40
choosetheone <s264157250r@gmail.com>
2020-05-17 21:33:35 +0300

    add 7

--- probe.py
+++ probe.py
@@ -207,31 +207,159 @@
 # your_favorite_color('green', color='red', car='toyota')
 
 
-class Car:
-    wheels_number = 4
+# class Car:
+#     wheels_number = 4
+#
+#     def __init__(self, name, color, year, is_crashed):
+#         self.name = name
+#         self.color = color
+#         self.year = year,
+#         self.is_crashed = is_crashed
+#
+#     def drive(self, city):
+#         print(self.name + ' is driving to ' + city)
+#
+#     def change_color(self, new_color):
+#         self.color = new_color
+#
+# opel_car = Car('Opel Tigra', 'red', 2018, False)
+# opel_car.drive('Paris')
+#
+# mazda_car = Car('Mazda CX7', 'black', 2015, True)
+# mazda_car.drive('London')
+# mazda_car.change_color('yellow')
+# print(mazda_car.color)
+#
+# print(opel_car.wheels_number)
+# print(opel_car.name)
+# print(opel_car.year)
+# print(opel_car.is_crashed)
+# print(opel_car.color)
 
-    def __init__(self, name, color, year, is_crashed):
+# class Circle:
+#     pi = 3.14
+#
+#     def __init__(self, radius=1):
+#         self.radius = radius
+#         self.circumference = 2 * Circle.pi * self.radius
+#
+#     def get_area(self):
+#         return self.pi * (self.radius ** 2)
+
+# def get_circumference(self):
+#     return 2 * self.pi * self.radius
+
+# circle_1 = Circle(5)
+# print(circle_1.get_area())
+# print(circle_1.circumference)
+# print(circle_1.get_circumference())
+# circle_2 = Circle(3)
+# print(circle_2.get_area())
+# print(circle_2.get_circumference())
+# circle_3 = Circle(5)
+# circle_area = circle_3.get_area()
+# print(circle_3.get_circumference())
+# print(circle_area)
+
+
+# class Car:
+#     wheels_number = 4
+#
+#     def __init__(self, name, color, year, is_crashed):
+#         self.name = name
+#         self.color = color
+#         self.year = year
+#         self.is_crashed = is_crashed
+#         print('Car is created')
+#
+#     def drive(self, city):
+#         print(self.name + ' is driving to ' + city)
+#
+#     def change_color(self, new_color):
+#         self.color = new_color
+#         print('color is changed to ' + new_color)
+#
+#
+# class Truck(Car):
+#     wheels_number = 6
+#
+#     def __init__(self, name, color, year, is_crashed):
+#         Car.__init__(self, name, color, year, is_crashed)
+#         print('Track is created')
+#
+#     def drive(self, city):
+#         print('Truck ' + self.name + ' is driving to ' + city)
+#
+#     def load_cargo(self, weight):
+#         print('The cargo is loaded. Weight is ' + str(weight) + ' kg')
+#
+#
+#
+# man_truck = Truck('Man', 'cyan', 1996, True)
+#
+# man_truck.drive('New York')
+# print(man_truck.wheels_number)
+# print(man_truck.color)
+# man_truck.change_color('black')
+# print(man_truck.color)
+# man_truck.load_cargo(2000)
+
+
+# polymorphism
+
+class Animal:
+    def __init__(self, name):
         self.name = name
-        self.color = color
-        self.year = year,
-        self.is_crashed = is_crashed
 
-    def drive(self):
-        print('Car is driving')
+    def speak(self):
+        raise NotImplementedError('Class successor must implement this method')
 
-opel_car = Car('Opel Tigra', 'red', 2018, False)
-opel_car.drive()
-print(opel_car.wheels_number)
-print(opel_car.name)
-print(opel_car.year)
-print(opel_car.is_crashed)
-print(opel_car.color)
+class Dog(Animal):
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        print(self.name + ' is saying woof')
 
 
+class Cat(Animal):
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        print(self.name + ' is saying meow')
+
+
+class Mouse(Animal):
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        print(self.name + ' is saying pe-pe-pe')
+
+class Fish(Animal):
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        print(self.name + ' is saying nothink')
 
 
 
+spike = Dog('Spike')
+tom = Cat('Tom')
+jerry = Mouse('Jerry')
+freddy = Fish('Freddy')
 
+pet_list = [spike, tom, jerry, freddy]
 
+for pet in pet_list:
+    pet.speak()
 
+def pet_voice(pet):
+    pet.speak()
 
+pet_voice(spike)
+pet_voice(tom)
+pet_voice(jerry)
+pet_voice(freddy)
