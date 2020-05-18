651ec207dd531119cfe6af9034b927c16e363b0a
choosetheone <s264157250r@gmail.com>
2020-05-18 18:21:03 +0300

    continue

--- customs_stone.py
+++ customs_stone.py
@@ -3,5 +3,18 @@ sum_of_specification = input('Введите сумму предложения: 
 weight_of_specification = input('Введите вес по спецификации: ')
 sum_of_transport = input('Введите общую сумму транспорта: ')
 sum_of_customs_clearance = input('Введите стоимость таможенного оформления: ')
+exchange_rates = input('Введите курс евро в рублях: ')
 
+print(sum_of_specification)
+print(sum_of_transport)
+print(weight_of_specification)
+print(sum_of_customs_clearance)
+
+# расчет на одно авто
+sum_of_trucks = int(weight_of_specification) / 22000
+print(sum_of_trucks)
+cost_invoice = int(sum_of_specification) / int(sum_of_trucks)
+print(cost_invoice)
+sum_of_insurance = int(cost_invoice) * 0.2 / 100
+print(sum_of_insurance)
 
