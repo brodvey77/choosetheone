f4cc1fad50038388eb74668304b08be9ef05cf82
choosetheone <s264157250r@gmail.com>
2020-05-18 17:47:28 +0300

    добавил переменные

---
+++ customs_stone.py
@@ -0,0 +1,7 @@
+
+sum_of_specification = input('Введите сумму предложения: ')
+weight_of_specification = input('Введите вес по спецификации: ')
+sum_of_transport = input('Введите общую сумму транспорта: ')
+sum_of_customs_clearance = input('Введите стоимость таможенного оформления: ')
+
+
