77223e1218cd4cd77a80549b5595aa9cb39a93ec
choosetheone <s264157250r@gmail.com>
2020-05-21 15:33:13 +0300

    first done

--- customs_stone.py
+++ customs_stone.py
@@ -1,3 +1,4 @@
+import math
 
 sum_of_specification = input('Введите сумму предложения: ')
 weight_of_specification = input('Введите вес по спецификации: ')
@@ -6,15 +7,15 @@ sum_of_customs_clearance = input('Введите стоимость таможе
 exchange_rates = input('Введите курс евро в рублях: ')
 
 print('сумма предложения - ' + sum_of_specification)
-print(sum_of_transport)
-print(weight_of_specification)
-print(sum_of_customs_clearance)
+print('сумма транспорта - ' + sum_of_transport)
+print('Вес по спецификации - ' + weight_of_specification)
+print('Стоимость таможенного оформления - ' + sum_of_customs_clearance)
 
 # расчет на одно авто
-sum_of_trucks = int(weight_of_specification) / 22000
-print('Колличество авто {}'.format(sum_of_trucks))
-# cost_invoice = int(sum_of_specification) / int(sum_of_trucks)
-# print(cost_invoice)
+sum_of_trucks = math.ceil(int(weight_of_specification) / 22000)
+print('Колличество авто - {}'.format(int(sum_of_trucks)))
+cost_invoice = math.ceil(int(sum_of_specification) / sum_of_trucks)
+print('Сумма инвойса - ' + str(cost_invoice) + 'евро')
 # sum_of_insurance = int(cost_invoice) * 0.2 / 100
 # print(sum_of_insurance)
 
