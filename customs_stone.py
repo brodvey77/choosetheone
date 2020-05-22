734afb1be2c4c371ff7ff5624328e15ba2d5b3dd
choosetheone <s264157250r@gmail.com>
2020-05-22 17:46:24 +0300

    добавил расчет на одно авто

--- customs_stone.py
+++ customs_stone.py
@@ -1,6 +1,5 @@
 import math
 # Ввод данных
-
 sum_of_specification = input('Введите сумму предложения: ')
 weight_of_specification = input('Введите вес по спецификации: ')
 sum_of_transport = input('Введите общую сумму транспорта: ')
@@ -8,10 +7,10 @@ sum_of_customs_clearance = input('Введите стоимость таможе
 exchange_rates = input('Введите курс евро в рублях: ')
 
 # проверка
-print('сумма предложения - ' + sum_of_specification + ' евро')
-print('сумма транспорта - ' + sum_of_transport + ' евро')
-print('Вес по спецификации - ' + weight_of_specification + ' кг')
-print('Стоимость таможенного оформления - ' + sum_of_customs_clearance + ' евро')
+# print('сумма предложения - ' + sum_of_specification + ' евро')
+# print('сумма транспорта - ' + sum_of_transport + ' евро')
+# print('Вес по спецификации - ' + weight_of_specification + ' кг')
+# print('Стоимость таможенного оформления - ' + sum_of_customs_clearance + ' евро')
 
 # расчет на одно авто
 sum_of_trucks = math.ceil(int(weight_of_specification) / 22000)
@@ -24,12 +23,10 @@ transport_to_board = math.ceil(int(sum_of_transport) * 55 / 100)
 print('Сумма транспорта до границы с ТС - ' + str(transport_to_board) + ' евро')
 
 # Расчет таможенной стоимости
-
 customs_value = cost_invoice + transport_to_board + sum_of_insurance
 print('Таможенная стоимость - ' + str(customs_value) + ' евро')
 
 # Расчет таможенных платежей
-
 custom_duty = math.ceil(customs_value * 14 / 100)
 print('Пошлина - ' + str(custom_duty))
 vat = math.ceil((customs_value + custom_duty) * 20 / 100)
@@ -37,10 +34,9 @@ print('НДС - ' + str(vat))
 
 
 # Расчет таможенного сбора
-
 customs_value_ru = math.ceil(customs_value * int(exchange_rates))
-print('Таможенная стоимость в рублях - ' + str(customs_value_ru) + ' рублей')
-
+# print('Таможенная стоимость в рублях - ' + str(customs_value_ru) + ' рублей')
+customs_fee = 0
 if customs_value_ru in range(0, 200001):
     customs_fee = 375
     print('Таможенный сбор - ' + str(customs_fee) + ' рублей')
@@ -62,3 +58,33 @@ elif customs_value_ru in range(5000001, 10000001):
 elif customs_value_ru > 10000001:
     customs_fee = 22500
     print('Таможенный сбор - ' + str(customs_fee) + ' рублей')
+
+
+# Сумма таможенных платежей
+customs_fee_eur = math.ceil(customs_fee / int(exchange_rates))
+sum_of_customs_payments = custom_duty + vat + customs_fee_eur
+# print(sum_of_customs_payments)
+
+
+# Вывод данных
+print('Расчет на одно авто:')
+# print('''
+# {0:12.3f} {1:12.3f}
+# {2:12.3f} {3:12.3f}
+# {4:12.3f} {5:12.3f}
+# {6:12.3f} {7:12.3f}
+# {8:12.3f} {9:12.3f}
+# {10:12.3f} {11:12.3f}
+# '''.format('Сумма инвойса', cost_invoice, 'Транспорт', sum_of_transport,
+#            'Страховка', sum_of_insurance, 'Таможенные платежи', sum_of_customs_payments,
+#            'Таможенное оформление', sum_of_customs_clearance))
+
+sum_of_one_track = cost_invoice + int(sum_of_transport) + sum_of_insurance + sum_of_customs_payments + int(sum_of_customs_clearance)
+
+print('Сумма инвойса - ' + str(cost_invoice) + ' евро')
+print('сумма транспорта - ' + sum_of_transport + ' евро')
+print('Сумма страховки - ' + str(sum_of_insurance) + ' евро')
+print('Сумма таможенных платежей - ' + str(sum_of_customs_payments))
+print('Стоимость таможенного оформления - ' + sum_of_customs_clearance + ' евро')
+print('Итого на одно авто - ' + str(sum_of_one_track) + ' евро')
+
