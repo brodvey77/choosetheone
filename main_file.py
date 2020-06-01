3e7a470794a4959fb7f3da0abe6d46fbe88ae771
brodvey77 <s264157250r@gmail.com>
2020-06-01 21:22:18 +0300

    решение задачи юдеми к 7.2

---
+++ main_file.py
@@ -0,0 +1,11 @@
+# from utils import get_favorite_color, get_favorite_number
+
+import utils
+#
+color = utils.get_favorite_color()
+#
+number = utils.get_favorite_number()
+#
+print(color)
+print(number)
+
