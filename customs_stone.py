61b5fdb4084b2fcf060f6d4d9594df3c9cbe3f06
choosetheone <brodvey77@aol.com>
2020-07-29 08:54:42 +0300

    add format2

--- customs_stone.py
+++ customs_stone.py
@@ -19,30 +19,33 @@ exchange_rates = input('Введите курс евро в рублях: ')
 
 # расчет на одно авто
 sum_of_trucks = math.ceil(int(weight_of_specification) / 22000)
-print('Количество авто - {} машин'.format(int(sum_of_trucks)))
+print(f'Количество авто - {int(sum_of_trucks)} машин')
 cost_invoice = math.ceil(int(sum_of_specification) / sum_of_trucks)
-print('Сумма инвойса - ' + str(cost_invoice) + ' евро')
+print(f'Сумма инвойса - {str(cost_invoice)} евро')
 sum_of_insurance = math.ceil(int(cost_invoice) * 0.2 / 100)
-print('Сумма страховки - ' + str(sum_of_insurance) + ' евро')
-print('========================================================')
+print(f'Сумма страховки - {str(sum_of_insurance)} евро')
+
+
+def print_delimetr():   # функция разделителя
+    print('========================================================')
 
 #  создать функцию выбора процентной ставки
 
 transport_to_board = math.ceil(int(sum_of_transport) * 40 / 100)
-print('Сумма транспорта до границы с ТС - ' + str(transport_to_board) + ' евро')
-print('========================================================')
+print(f'Сумма транспорта до границы с ТС - {str(transport_to_board)} евро')
+print_delimetr()
 
 # Расчет таможенной стоимости
 customs_value = cost_invoice + transport_to_board + sum_of_insurance
-print('Таможенная стоимость - ' + str(customs_value) + ' евро')
-print('========================================================')
+print(f'Таможенная стоимость - {str(customs_value)} евро')
+print_delimetr()
 
 # Расчет таможенных платежей
 custom_duty = math.ceil(customs_value * 14 / 100)
-print('Пошлина - ' + str(custom_duty))
+print(f'Пошлина - {str(custom_duty)}')
 vat = math.ceil((customs_value + custom_duty) * 20 / 100)
-print('НДС - ' + str(vat))
-print('========================================================')
+print(f'НДС - {str(vat)}')
+
 
 # Расчет таможенного сбора
 customs_value_ru = math.ceil(customs_value * int(exchange_rates))
@@ -50,38 +53,37 @@ customs_value_ru = math.ceil(customs_value * int(exchange_rates))
 customs_fee = 0
 if customs_value_ru in range(0, 200001):
     customs_fee = 375
-    print('Таможенный сбор - ' + str(customs_fee) + ' рублей')
+    print(f'Таможенный сбор - {str(customs_fee)} рублей')
 elif customs_value_ru in range(200001, 450001):
     customs_fee = 750
-    print('Таможенный сбор - ' + str(customs_fee) + ' рублей')
+    print(f'Таможенный сбор - {str(customs_fee)} рублей')
 elif customs_value_ru in range(450001, 1000201):
     customs_fee = 1500
-    print('Таможенный сбор - ' + str(customs_fee) + ' рублей')
+    print(f'Таможенный сбор - {str(customs_fee)} рублей')
 elif customs_value_ru in range(1000201, 2500001):
     customs_fee = 4125
-    print('Таможенный сбор - ' + str(customs_fee) + ' рублей')
+    print(f'Таможенный сбор - {str(customs_fee)} рублей')
 elif customs_value_ru in range(2500001, 5000001):
     customs_fee = 5625
-    print('Таможенный сбор - ' + str(customs_fee) + ' рублей')
+    print(f'Таможенный сбор - {str(customs_fee)} рублей')
 elif customs_value_ru in range(5000001, 10000001):
     customs_fee = 15000
-    print('Таможенный сбор - ' + str(customs_fee) + ' рублей')
+    print(f'Таможенный сбор - {str(customs_fee)} рублей')
 elif customs_value_ru > 10000001:
     customs_fee = 22500
-    print('Таможенный сбор - ' + str(customs_fee) + ' рублей')
-print('========================================================')
+    print(f'Таможенный сбор - {str(customs_fee)} рублей')
+print_delimetr()
 
 # Сумма таможенных платежей
 customs_fee_eur = math.ceil(customs_fee / int(exchange_rates))
 sum_of_customs_payments = custom_duty + vat + customs_fee_eur
 # print(sum_of_customs_payments)
-print('========================================================')
 
 # Вывод данных
 print('Расчет на одно авто:')
 
 sum_of_one_track = cost_invoice + int(sum_of_transport) + sum_of_insurance + sum_of_customs_payments + int(sum_of_customs_clearance)
-print('========================================================')
+print_delimetr()
 pprint('Сумма инвойса - ' + str(cost_invoice) + ' евро')
 pprint('сумма транспорта - ' + sum_of_transport + ' евро')
 pprint('Сумма страховки - ' + str(sum_of_insurance) + ' евро')
@@ -91,7 +93,7 @@ pprint('Итого на одно авто - ' + str(sum_of_one_track) + ' евр
 
 pprint('Общий расчет:')
 pprint('Итого - ' + str(sum_of_one_track * sum_of_trucks) + ' евро')
-print('========================================================')
+print_delimetr()
 
 
 
