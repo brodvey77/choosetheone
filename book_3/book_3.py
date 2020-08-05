4aa46de1624d26a011ba452ebe9a251233a18aaa
choosetheone <brodvey77@aol.com>
2020-08-05 14:40:17 +0300

    add

--- book_3/book_3.py
+++ book_3/book_3.py
@@ -1,21 +1,59 @@
 
 
+# phrase = "Don't panic!"
+# plist = list(phrase)
+# print(phrase)
+# print(plist)
+#
+# for i in range(4):
+#     plist.pop()
+#
+# print(plist)
+# plist.pop(0)
+# print(plist)
+# plist.remove("'")
+# print(plist)
+# plist.extend([plist.pop(), plist.pop()])
+# plist.insert(2, plist.pop(3))
+# print(plist)
+# new_phrase = ''.join(plist)
+# print(new_phrase)
+
+# first = [1, 2, 3, 4, 5]
+# print(first)
+# second_ = first
+# print(second_)
+# second_.append(6)
+# print(second_)
+# print(first)
+#
+# third_ = second_.copy()
+# print(third_)
+# third_.append(7)
+# print(third_)
+# print(second_)
+
+# book = "The Hitchhiker's Guide to the Galaxy"
+# book_list = list(book)
+# print(book_list)
+# print(book_list[:3])
+# print(''.join(book_list[:3]))
+# print(''.join(book_list[-6:]))
+# backwards = book_list[::-1]
+# print(''.join(backwards))
+# every_other = book_list[::2]
+# print(''.join(every_other))
+# print(''.join(book_list[4:14:]))
+# print(''.join(book_list[13:3:-1]))
+
+
+
 phrase = "Don't panic!"
 plist = list(phrase)
 print(phrase)
 print(plist)
 
-for i in range(4):
-    plist.pop()
+
 
 print(plist)
-plist.pop(0)
-print(plist)
-plist.remove("'")
-print(plist)
-plist.extend([plist.pop(), plist.pop()])
-plist.insert(2, plist.pop(3))
-print(plist)
-new_phrase = ''.join(plist)
-print(new_phrase)
-
+print(newphrase)
