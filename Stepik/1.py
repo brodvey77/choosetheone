5749befd8228883c51a621ea00a473e1b5cd3e08
brodv <s264157250r@gmail.com>
2020-09-20 14:02:30 +0300

    фвв

---
+++ Stepik/1.py
@@ -0,0 +1,15 @@
+# print('a', 'b', 'c', sep='*')
+# print('d', 'e', 'f', sep='**', end='')
+# print('g', 'h', 'i', sep='+', end='%')
+# print('j', 'k', 'l', sep='-', end='\n')
+# print('m', 'n', 'o', sep='/', end='!')
+# print('p', 'q', 'r', sep='1', end='%')
+# print('s', 't', 'u', sep='&', end='\n')
+# print('v', 'w', 'x', sep='%')
+# print('y', 'z', sep='/', end='!')
+
+# print('I', 'like', 'Python', sep='***', end='')
+#
+
+name = input()
+print('Привет, ' + name + '!', end='')
