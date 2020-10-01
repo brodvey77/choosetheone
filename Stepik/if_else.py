33698c822673cc90b7541d7b06a0ab96131b21d6
brodvey77 <s264157250r@gmail.com>
2020-10-01 16:43:09 +0300

    add quest

---
+++ Stepik/if_else.py
@@ -0,0 +1,42 @@
+
+# language = input('input language >> ')
+# if language == 'Python':
+#     print('Yes')
+# else:
+#     print('No')
+
+# number = int(input('input number two digit >> '))
+# last_digit = number % 10
+# first_digit = number // 10
+# if last_digit == first_digit:
+#     print('Yes')
+# else:
+#     print('No')
+
+# number_1, number_2, number_3 = int(input()), int(input()), \
+#                                int(input())
+# counter = 0
+# if number_1 % 2 == 0:
+#     counter = counter + 1
+# if number_2 % 2 == 0:
+#     counter = counter + 1
+# if number_3 % 2 == 0:
+#     counter = counter + 1
+# print(counter)
+
+# password_1 = (input())
+# password_2 = (input())
+#
+# if password_1 == password_2:
+#     print('Пароль принят')
+# else:
+#     print('Пароль не принят')
+
+# print('Пароль принят' if input() == input() else 'Пароль не принят')
+
+# number = int(input())
+# if number % 2 == 0:
+#     print('Четное')
+# else:
+#     print('Нечетное')
+# print('Четное' if not int(input()) % 2 else 'Нечетное')
