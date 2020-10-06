6aab68f2ecb64fc03d6566349abd6ad07d83770a
brodvey77 <s264157250r@gmail.com>
2020-10-06 11:35:24 +0300

    bot add echo

--- Stepik/if_else.py
+++ Stepik/if_else.py
@@ -60,6 +60,30 @@
 #
 # print('Доступ разрешен' if  int(input()) >= 18 else 'Доступ запрещен')
 
+# number_1 = int(input())
+# number_2 = int(input())
+# number_3 = int(input())
+#
+# if number_2 - number_1 == number_3 - number_2:
+#     print('YES')
+# else:
+#     print('NO')
+
+# number_1 = int(input())
+# number_2 = int(input())
+#
+# if number_1 > number_2:
+#     print(number_1)
+# else:
+#     print(number_2)
+
+# a = int(input())
+# b = int(input())
+# print(a if a < b else b)
+
+
+
+
 
 
 
