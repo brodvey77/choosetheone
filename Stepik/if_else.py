b4c47f4cc794f7c86d783d3f5aa2b7b5b0ba7785
brodvey77 <s264157250r@gmail.com>
2020-10-29 12:51:12 +0300

    add new1

--- Stepik/if_else.py
+++ Stepik/if_else.py
@@ -313,3 +313,26 @@
 #     print('желтый')
 # else:
 #     print('ошибка цвета')
+
+# number = int(input())
+#
+# if number == 0:
+#     print('зеленый')
+# elif 1 <= number <= 10 and number % 2 != 0:
+#     print('красный')
+# elif 1 <= number <= 10 and number % 2 == 0:
+#     print('черный')
+# elif 11 <= number <= 18 and number % 2 != 0:
+#     print('черный')
+# elif 11 <= number <= 18 and number % 2 == 0:
+#     print('красный')
+# elif 19 <= number <= 28 and number % 2 != 0:
+#     print('красный')
+# elif 19 <= number <= 28 and number % 2 == 0:
+#     print('черный')
+# elif 29 <= number <= 36 and number % 2 != 0:
+#     print('черный')
+# elif 29 <= number <= 36 and number % 2 == 0:
+#     print('красный')
+# else:
+#     print('ошибка ввода')
