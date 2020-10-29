6173469d1f1a4184c781f7d8eb94b7d5cce7313c
brodvey77 <s264157250r@gmail.com>
2020-10-29 16:03:08 +0300

    add new2

--- Stepik/if_else.py
+++ Stepik/if_else.py
@@ -336,3 +336,35 @@
 #     print('красный')
 # else:
 #     print('ошибка ввода')
+
+# a1 = int(input())
+# b1 = int(input())
+# a2 = int(input())
+# b2 = int(input())
+#
+# if a1 < a2 and b1 > b2:
+#     print(a2, b2)
+# elif a1 < a2 and b1 == b2:
+#     print(a2, b2)
+# elif a1 == a2 and b1 > b2:
+#     print(a1, b2)
+# elif a1 == a2 and b1 == b2:
+#     print(a1, b2)
+#
+# elif a1 == a2 and b1 < b2:
+#     print(a1, b1)
+# elif a1 > a2 and b1 == b2:
+#     print(a1, b1)
+# elif a1 > a2 and b1 < b2:
+#     print(a1, b1)
+#
+# elif a1 < a2 == b1 and b1 < b2:
+#     print(b1)
+# elif a1 > a2 and b1 > b2 and a1 == b2:
+#     print(a1)
+# elif a1 < a2 < b1 < b2:
+#     print(a2, b1)
+# elif a2 < a1 < b2 < b1:
+#     print(a1, b2)
+# else:
+#     print('пустое множество')
