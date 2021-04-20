721bf3f362b12218bcea74ed977951da8855a34a
brodvey77 <s264157250r@gmail.com>
2021-04-20 12:16:11 +0300

    update 22

---
+++ Stepik/typa_data.py
@@ -0,0 +1,145 @@
+# a = float(input())
+# b = float(input())
+#
+# s = 1 / 2 * a * b
+#
+# print(s)
+
+
+# s = float(input())
+# v1 = float(input())
+# v2 = float(input())
+#
+# t = s / (v1 + v2)
+#
+# print(t)
+
+# num = float(input())
+#
+# if num == 0:
+#     print('Обратного числа не существует')
+# else:
+#     print(num ** -1)
+
+# f = float(input())
+#
+# c = 5 / 9 * (f - 32)
+#
+# print(c)
+
+
+# f = int(input())
+# age = 0
+#
+# if f <= 1:
+#     age = f * 10.5
+# elif f == 2:
+#     age = int(f * 10.5)
+# else:
+#     age = (f - 2) * 4 + 21
+#
+# print(age)
+
+
+# num = float(input())
+# num2 = int(num * 10)
+# num2 = num2 % 10
+#
+# print(num2)
+
+
+# num = float(input())
+# num2 = num % 1
+#
+# print(num2)
+
+# a = float(input())
+# b = int(a)
+# print(a - b)
+
+# num = max(1, 3, -5, 7) + min(-3, 6, -8, -1) + abs(-17)
+# print(num)
+
+
+# num1 = int(input())
+# num2 = int(input())
+# num3 = int(input())
+# num4 = int(input())
+# num5 = int(input())
+#
+# min_num = min(num1, num2, num3, num4, num5)
+# max_num = max(num1, num2, num3, num4, num5)
+#
+# print('Наименьшее число = ', min_num)
+# print('Наибольшее число = ', max_num)
+
+
+# num1 = int(input())
+# num2 = int(input())
+# num3 = int(input())
+#
+# n3 = min(num1, num2, num3)
+# n1 = max(num1, num2, num3)
+#
+# sum_num = num1 + num2 + num3
+#
+# n2 = sum_num - n3 - n1
+#
+# print(n1)
+# print(n2)
+# print(n3)
+
+
+# num = int(input())
+#
+# n3 = num % 10
+# n2 = num % 100 // 10
+# n1 = num % 1000 // 100
+#
+# max_num = max(n1, n2, n3)
+# min_num = min(n1, n2, n3)
+#
+# average = n1 + n2 + n3 - max_num - min_num
+#
+# if max_num - min_num == average:
+#     print('Число интересное')
+# else:
+#     print('Число неинтересное')
+
+# a1 = float(input())
+# a2 = float(input())
+# a3 = float(input())
+# a4 = float(input())
+# a5 = float(input())
+#
+# if a1 < 0:
+#     a1 = abs(a1)
+# if a2 < 0:
+#     a2 = abs(a2)
+# if a3 < 0:
+#     a3 = abs(a3)
+# if a4 < 0:
+#     a4 = abs(a4)
+# if a5 < 0:
+#     a5 = abs(a5)
+#
+# sum_a = a1 + a2 + a3 + a4 + a5
+# print(sum_a)
+
+# a = abs(float(input()))
+# b = abs(float(input()))
+# c = abs(float(input()))
+# x = abs(float(input()))
+# y = abs(float(input()))
+#
+# print(a + b + c + x + y)
+
+
+# p1 = int(input())
+# p2 = int(input())
+# q1 = int(input())
+# q2 = int(input())
+#
+# print(abs(p1 - q1) + abs(p2 - q2))
+
+
