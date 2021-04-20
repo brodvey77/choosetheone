eb5d7884ed208c5246733e37ca608c51a6542564
brodvey77 <s264157250r@gmail.com>
2021-04-20 14:30:36 +0300

    update 24

--- Stepik/typa_data.py
+++ Stepik/typa_data.py
@@ -223,4 +223,12 @@
 #
 # print(s)
 
+# STRINS!!!!
+
+# a = '"Python is a great language!"'
+# b = ', said Fred.'
+# c = '"I don\'t ever remember having this much fun before."'
+#
+# print(a + b + c)
+
 
