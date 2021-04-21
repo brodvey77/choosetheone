d35e1361d79f43fff3d6e4afd47a540eb771c567
brodvey77 <s264157250r@gmail.com>
2021-04-21 14:14:19 +0300

    add1

---
+++ Stepik/cycle.py
@@ -0,0 +1,64 @@
+# for название_переменной_цикла in range(количество повторений):
+#     блок кода
+
+# for i in range(2):
+#     print('Привет')
+
+# for i in range(5):
+#     num = int(input())
+#     print('Квадрат вашего числа равен:', num * num)
+# print('Цикл завершен')
+
+# for i in range(10):
+#     print('Python is awesome!')
+
+# text = input()
+# count = int(input())
+#
+# for i in range(count):
+#     print(text)
+
+# for i in range(6):
+#     print('AAA')
+# for i in range(5):
+#     print('BBBB')
+# print('E')
+# for i in range(9):
+#     print('TTTTT')
+# print('G')
+
+# n = int(input())
+# if 1 <= n <= 20:
+#     for i in range(n):
+#         print('*' * 19)
+# else:
+#     print('Ваше число должно быть от 1 до 20')
+
+
+# print('*******************\n' * int(input()))
+
+# text = input()
+#
+# for i in range(10):
+#     print(i, text)
+
+# n = int(input())
+#
+# for i in range(n+1):
+#     print('Квадрат числа', i, 'равен', i * i)
+
+# n = int(input())
+#
+# if n >= 2:
+#     for i in range(n):
+#         print('*' * (n - i))
+# else:
+#     print('Число должно быть от 2 - ух и выше')
+
+# m = int(input())
+# p = int(input())
+# n = int(input())
+#
+# for i in range(n):
+#     print(i + 1, m * (p / 100 + 1) ** i)
+
