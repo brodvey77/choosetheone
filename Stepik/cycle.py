b420decef78ef895a9e57ab95d228f70b73077b8
brodvey77 <s264157250r@gmail.com>
2021-05-13 15:03:23 +0300

    add 9

--- Stepik/cycle.py
+++ Stepik/cycle.py
@@ -366,17 +366,45 @@
 #     num = num // 10
 #     print(last_digit, end='')
 
-num = int(input())
+# num = int(input())
+# largest = 0
+# smallest = 9
+#
+# while num != 0:
+#     last_digit = num % 10
+#     if last_digit > largest:
+#         largest = last_digit
+#     if last_digit < smallest:
+#         smallest = last_digit
+#     num = num // 10
+# print('Максимальная цифра равна', largest)
+# print('Минимальная цифра равна', smallest)
 
-largest = 0
 
-while num != 0:
-    last_digit = num % 10
-    if last_digit > largest:
-        largest = last_digit
-    if last_digit < largest:
-        smallest = last_digit
+# num = int(input())
+# sum_of_digit = 0
+# multiplication = 1
+# total_digit = 0
+# average = 0
+# first_digit = 0
+# last_digit = 0
+# sum_of_last_anf_first_digit = 0
+#
+# while num != 0:
+#     digit = num % 10
+#     total_digit += 1
+#     sum_of_digit += digit
+#     multiplication *= digit
+#     first_digit = num % 10
+#     if total_digit == 1:
+#         last_digit = digit
+#     num = num // 10
+#
+# print(sum_of_digit)
+# print(total_digit)
+# print(multiplication)
+# print(sum_of_digit / total_digit)
+# print(first_digit)
+# print(first_digit + last_digit)
 
-    num = num // 10
-print('Максимальная цифра равна', largest)
-print('Минимальная цифра равна', smallest)
+
