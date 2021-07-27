41d5a68247dadf91f5c715620c53eb8a7bad935b
brodvey77 <s264157250r@gmail.com>
2021-07-27 10:41:58 +0300

    37

--- Stepik/lists.py
+++ Stepik/lists.py
@@ -599,18 +599,21 @@ import smtplib
 # sum_list = sum([int(i) for i in string.split()])
 # print('+'.join(list), '=', sum_list, sep='')
 
-# stroka = str(input())
-#
-# list = [i for i in stroka.split('-')]
-#
-# if ''.join(list).isdigit():
-#     list_2 = [int(i) for i in stroka.split('-')]
-#     if list_2[0] == 7:
-#         del list_2[0]
-#     if 99 < list_2[0] <= 999 and 99 < list_2[1] <= 999 and 999 < list_2[2] <= 9999:
-#         print('YES')
-# else:
-#     ('NO')
+stroka = str(input())
+
+list = [i for i in stroka.split('-')]
+
+if list[0] == '7':
+    del list[0]
+
+if ''.join(list).isdigit():
+    list_2 = [int(i) for i in list]
+    if 99 < list_2[0] <= 999 and 99 < list_2[1] <= 999 and 999 < list_2[2] <= 9999:
+        print('YES')
+    else:
+        print('NO')
+else:
+    print('NO')
 
 # spisok1=[i for i in input().split('-')]
 # spisok2=[len(i) for i in spisok1 if i.isdigit()]
