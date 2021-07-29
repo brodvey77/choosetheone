0316870e4c6a8a1cdc611e6dee42af8def7bb7ae
brodvey77 <s264157250r@gmail.com>
2021-07-29 14:27:20 +0300

    add qm

--- Stepik/functions.py
+++ Stepik/functions.py
@@ -205,7 +205,67 @@
 # # вызываем функцию
 # print(merge(numbers1, numbers2))
 
+# n = int(input())
+#
+# list = []
+#
+# for i in range(n):
+#     list1 = [int(c) for c in input().split()]
+#     list = sorted(list + list1)
+#
+# print(list)
 
 
+# def quick_merge(list1, list2):
+#     result = []
+#     p1 = 0  # указатель на первый элемент списка list1
+#     p2 = 0  # указатель на первый элемент списка list2
+#     while p1 < len(list1) and p2 < len(list2):  # пока не закончился хотя бы один список
+#         if list1[p1] <= list2[p2]:
+#             result.append(list1[p1])
+#             p1 += 1
+#         else:
+#             result.append(list2[p2])
+#             p2 += 1
+#     if p1 < len(list1):   # прицепление остатка
+#         result += list1[p1:]
+#     if p2 < len(list2):
+#         result += list2[p2:]
+#     return result
+#
+# n = int(input())
+# numbers1 = [int(c) for c in input().split()]
+# for _ in range(1, n):
+#     numbers1 = quick_merge(numbers1, [int(c) for c in input().split()])
+# print(*numbers1)
 
 
+# def quick_merge(list1, list2):
+#     result = []
+#
+#     p1 = 0  # указатель на первый элемент списка list1
+#     p2 = 0  # указатель на первый элемент списка list2
+#
+#     while p1 < len(list1) and p2 < len(list2):  # пока не закончился хотя бы один список
+#         if list1[p1] <= list2[p2]:
+#             result.append(list1[p1])
+#             p1 += 1
+#         else:
+#             result.append(list2[p2])
+#             p2 += 1
+#
+#     if p1 < len(list1):  # прицепление остатка
+#         result += list1[p1:]
+#     if p2 < len(list2):
+#         result += list2[p2:]
+#
+#     return result
+#
+# n = int(input())
+#
+# spisok = [int(c) for c in input().split()]
+#
+# for _ in range(n - 1):
+#     spisok = quick_merge(spisok, [int(c) for c in input().split()])
+#
+# print(*spisok)
