5cd55e7594c3132b23cb58f5b292b6ff72254a76
brodvey77 <s264157250r@gmail.com>
2021-09-14 11:31:34 +0300

    13.6_2

--- Stepik/functions.py
+++ Stepik/functions.py
@@ -533,3 +533,17 @@
 # # вызываем функцию
 # print(convert_to_python_case(txt))
 
+
+# # объявление функции
+# def get_middle_point(x1, y1, x2, y2):
+#     a = (x1 + x2) / 2
+#     b = (y1 + y2) / 2
+#     return a, b
+#
+# # считываем данные
+# x_1, y_1 = int(input()), int(input())
+# x_2, y_2 = int(input()), int(input())
+#
+# # вызываем функцию
+# x, y = get_middle_point(x_1, y_1, x_2, y_2)
+# print(x, y)
