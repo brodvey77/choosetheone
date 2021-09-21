38abc7df1511d50e36cbb67fc0ca724ec79a80bb
brodvey77 <s264157250r@gmail.com>
2021-09-21 16:05:24 +0300

    Caesar's cipher begin

---
+++ Stepik/Caesar's cipher.py
@@ -0,0 +1,16 @@
+# Caesar's cipher
+import time
+
+
+
+
+
+english_symbols = 'abcdefghijklmnopqrstuvwxyz'
+russian_symbols = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
+
+switch = input('Выберете шифровние (s) или дешифрование (d) ')
+time.sleep(0.5)
+language = input('Какой язык выбрать? Английский (е) или Русский (r) ')
+time.sleep(0.5)
+step = int(input('Какой применить шаг сдвига? '))
+time.sleep(0.5)
