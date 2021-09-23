e75e80845b43f38ed8f644fd9272dda1ff3389cd
brodvey77 <s264157250r@gmail.com>
2021-09-23 11:48:57 +0300

    colorama learning

--- Stepik/Caesar's cipher.py
+++ Stepik/Caesar's cipher.py
@@ -1,15 +1,36 @@
 # Caesar's cipher
 import time
 
+english_symbols_lower = 'abcdefghijklmnopqrstuvwxyz'
+english_symbols_upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+russian_symbols_lower = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
+russian_symbols_upper = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 
 
-
-english_symbols = 'abcdefghijklmnopqrstuvwxyz'
-russian_symbols = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
-
-switch = input('Выберете шифровние (s) или дешифрование (d) ')
+switch = input('Выберете шифровние (s) или дешифрование (d) ').lower()
 time.sleep(0.5)
-language = input('Какой язык выбрать? Английский (е) или Русский (r) ')
+language = input('Какой язык выбрать? Английский (е) или Русский (r) ').lower()
 time.sleep(0.5)
-step = int(input('Какой применить шаг сдвига? '))
+key = int(input('Какой применить шаг сдвига? '))
 time.sleep(0.5)
+
+
+final_text = ''
+for i in text:
+    if i in russian_symbols_upper:
+        final_text += i[russian_symbols_upper] + key
+
+    if i in russian_symbols_lower:
+        final_text += i[russian_symbols_lower] + key
+
+
+
+
+
+
+# if switch == 's':
+#     text = input('Введите текст для шифрования: ')
+#     print(func_of_cipfer(text, language, key))
+# if switch == 'd':
+#     text = input('Введите текст для расшифровки: ')
+#     print(func_of_decipher(text, language, key))
--- Stepik/color_text.py
+++ Stepik/color_text.py
@@ -10,3 +10,45 @@
 # print(Style.DIM + 'back to dim now')
 # print(Style.BRIGHT + 'back to bright now')
 # print(Style.NORMAL + 'back to normal now')
+
+
+english_symbols_lower = 'abcdefghijklmnopqrstuvwxyz'
+english_symbols_upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+russian_symbols_lower = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
+russian_symbols_upper = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+key = 2
+#
+# switch = input('Выберете шифровние (s) или дешифрование (d) ').lower()
+# language = input('Какой язык выбрать? Английский (е) или Русский (r) ').lower()
+# key = int(input('Какой применить шаг сдвига? '))
+
+text = 'эюя'
+final_text = ''
+
+for i in text:
+    if i in russian_symbols_lower:
+        if russian_symbols_lower[russian_symbols_lower.index(i) + key] > len(russian_symbols_lower):
+
+    else:
+        print(russian_symbols_lower[russian_symbols_lower.index(i) + key], end='')
+    if i in russian_symbols_upper:
+        if
+    else:
+        print(russian_symbols_upper[russian_symbols_upper.index(i) + key], end='')
+
+
+
+
+
+# sdvig = int(input())
+# text = str(input())
+# char = 0
+# shifr = ''
+#
+# for i in text:
+#     char = ord(i) - sdvig
+#     if char < 97:
+#         shifr = 122 - (96 - char)
+#     else:
+#         shifr = char
+#     print(chr(shifr), end='')
