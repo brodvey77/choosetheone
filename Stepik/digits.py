da46aa219c845675e2d66b17031c3e2f2b9752de
brodvey77 <s264157250r@gmail.com>
2021-09-29 17:44:28 +0300

    system_1

--- Stepik/digits.py
+++ Stepik/digits.py
@@ -3,12 +3,23 @@
 #         print(n)
 
 
-# n=int(input("Начальное число: "))
-# m=int(input("В какую систему переводим: "))
-# s=""
-# vol=[0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"] #Это код из десятичной в любую
-# while n!=0:
-#     ch=n%m
-#     s=str(vol[ch])+s
-#     n//=m
+# n = int(input("Начальное число: "))
+# m = int(input("В какую систему переводим: "))
+# s = ""
+# vol = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]  # Это код из десятичной в любую
+# while n != 0:
+#     ch = n % m
+#     s = str(vol[ch]) + s
+#     n //= m
 # print(s)
+
+# num = int(input())
+#
+# two = bin(num)
+# eight = oct(num)
+# sixteen = hex(num)
+#
+# print(two[2:], eight[2:], sixteen[2:].upper(),sep='\n')
+
+
+
