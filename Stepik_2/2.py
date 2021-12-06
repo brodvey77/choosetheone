27c2024bda95ddc9ebe7e5ce1bd97758d36d8897
brodvey77 <s264157250r@gmail.com>
2021-12-06 11:08:07 +0300

    124

--- Stepik_2/2.py
+++ Stepik_2/2.py
@@ -660,6 +660,10 @@
 #
 # print(pascal(n))
 
-import math
+import speedtest
 
-n =
+speed = speedtest.Speedtest()
+download_speed = speed.download()
+upload_speed = speed.upload()
+print(f'The download speed is {download_speed}')
+print(f'The upload speed is {upload_speed}')
