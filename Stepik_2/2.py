503c865cb4b2ca12e46700cad4bfcc21439c8fe0
brodvey77 <s264157250r@gmail.com>
2021-12-29 14:28:51 +0300

    matrix_2_2

--- Stepik_2/2.py
+++ Stepik_2/2.py
@@ -1159,3 +1159,33 @@
 
 # print(mult)
 
+# n, m = int(input()), int(input())
+# matrix = []
+#
+# for i in range(n):
+#     row = [int(i) for i in input().split()]
+#     matrix.append(row)
+#
+# maximum = matrix[0][0]
+# a = 0
+# b = 0
+#
+# for i in range(n):
+#     for j in range(m):
+#         if matrix[i][j] > maximum:
+#             maximum = matrix[i][j]
+#             a = i
+#             b = j
+# print(a, b)
+
+
+# n, m = int(input()), int(input())
+# matrix = [[int(i) for i in input().split()] for _ in range(n)]
+# row, col = 0, 0
+#
+# for i in range(n):
+#     for j in range(m):
+#         if matrix[i][j] > matrix[row][col]:
+#             row, col = i, j
+#
+# print(row, col)
