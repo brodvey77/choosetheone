b21b19935c2a7077ccdba1585e10b40bef7c9e27
brodvey77 <s264157250r@gmail.com>
2022-01-11 14:06:39 +0300

    matrix_2_455

--- Stepik_2/2.py
+++ Stepik_2/2.py
@@ -1234,14 +1234,21 @@
 
 
 
+# n = int(input())
+# matrix = [input().split() for _ in range(n)]
+#
+# for i in range(n):
+#     matrix[i][i], matrix[n - i - 1][i] = matrix[n - i - 1][i], matrix[i][i]
+#
+# for r in range(n):
+#     for c in range(n):
+#         print(matrix[r][c], end=' ')
+#     print()
 
 
 
-
-
-
-
-
+n = int(input())
+matrix = [input().split() for _ in range(n)]
 
 
 
