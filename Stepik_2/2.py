6dc3c7e642bc75461aafb2a8d3e042c29a6a29f4
brodvey77 <s264157250r@gmail.com>
2022-01-18 16:21:52 +0300

    matrix_4_6_9

--- Stepik_2/2.py
+++ Stepik_2/2.py
@@ -1441,15 +1441,44 @@
 #         print(str(matrix[i][j]).ljust(3), end=' ')
 #     print()
 
-n, m = map(int, input().split())
-matrix = [[0] * m for _ in range(n)]
+# n, m = map(int, input().split())
+# matrix = [[0] * m for _ in range(n)]
+#
+# for i in range(n):
+#     for j in range(m):
+#         if i % 2 == 0:
+#             matrix[i][j] = str(i * m + j + 1).ljust(3)
+#         else:
+#             matrix[i][j] = str(i * m + m - j).ljust(3)
+#
+# for row in matrix:
+#     print(*row)
 
-for i in range(n):
-    for j in range(m):
-        if i % 2 == 0:
-            matrix[i][j] = str(i * m + j + 1).ljust(3)
-        else:
-            matrix[i][j] = str(i * m + m - j).ljust(3)
+# n, m = map(int, input().split())
+# matrix = [[0] * m for i in range(n)]
+# counter = 0
+# for q in range(n * m):
+#     for i in range(n):
+#         for j in range(m):
+#             if i + j == q:
+#                 counter += 1
+#                 matrix[i][j] = str(counter).ljust(3)
+#
+#
+# for row in matrix:
+#     print(*row)
 
-for row in matrix:
-    print(*row)
+# n, m = [int(i) for i in input().split()]
+#
+# matrix = [[1] * m for _ in range(n)]
+# row, col, diag = 0, 0, 0
+#
+# for i in range(1, n * m):
+#     col -= 1
+#     row += 1
+#     if col < 0 or row == n:
+#         diag += 1
+#         col = diag if diag < m else m - 1
+#         row = diag - col
+#
+#     matrix[row][col] += i
