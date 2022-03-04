1cd9faacee4dc39ca64ba6fd5c289f7cf64b3a53
brodvey77 <s264157250r@gmail.com>
2022-03-04 11:34:19 +0300

    040322

---
+++ Stepik_2/sets.py
@@ -0,0 +1,22 @@
+import math
+#
+# x = 36
+# y = 4
+#
+# a = math.sqrt(x / y)
+# b = math.sqrt(x * y)
+# c = math.sqrt(x - y)
+# d = math.sqrt(x + y)
+#
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+# c = int(input())
+# b = int(input())
+#
+# a = math.sqrt(c**2 - b**2)
+#
+# print(a)
+
