7785c2cf5a50e1f568a0fd70f7d53e858a11a4f2
brodvey77 <s264157250r@gmail.com>
2022-03-21 15:55:28 +0300

    210322

--- Stepik_2/sets.py
+++ Stepik_2/sets.py
@@ -172,7 +172,57 @@ import math
 #
 # print(*sorted(my_set))
 
+# set1 = {10, 20, 30, 40}
+# set2 = set(range(50))
+# print(set1.issubset(set2))
 
+# set1 = set('Stepik')
+# set2 = set('stepik')
+#
+# print(set1.issubset(set2))
 
+# word = 'beegeek'
+# set1 = set(word*3)
+# set2 = set(word[::-1]*2 + 'stepik')
+#
+# print(set1)
+# print(set2)
+#
+#
+# print(set2 < set1)
 
+# set1 = {1, 2, 3, 4, 5, 6, 7, 8}
+# list1 = [1, 2, 3, 4, 5]
+#
+# print(set1.issuperset(list1))
+
+# set1 = {'q', 'w', 'e', 'r', 't', 'y'}
+# list1 = ['y', 'w', 'r']
+#
+# print(set1 >= list1)
+
+# set1 = set(range(1, 10))
+# set2 = set(range(10, 20))
+#
+# print(set1.isdisjoint(set2))
+
+# a = set(input())
+# b = set(input())
+#
+# if a.isdisjoint(b):
+#     print('NO')
+# else:
+#     print('YES')
+
+# a = set(input())
+# b = set(input())
+#
+# if b.issubset(a):
+#     print('YES')
+# else:
+#     print('NO')
+
+# n = set(input().split()) & set(input().split()) - set(input().split())
+# result = sorted([int(i) for i in n], reverse=True)
+# print(*result)
 
