c6f81e5eec44aa8b8b89f8412b7cce4a23f7bb54
brodvey77 <s264157250r@gmail.com>
2022-03-28 15:13:37 +0300

    280322

--- Stepik_2/dict.py
+++ Stepik_2/dict.py
@@ -110,20 +110,100 @@
 #
 # print(*sorted(result))
 
-users = [{'name': 'Todd', 'phone': '551-1414', 'email': 'todd@gmail.com'},
-         {'name': 'Helga', 'phone': '555-1618'},
-         {'name': 'Olivia', 'phone': '449-3141', 'email': ''},
-         {'name': 'LJ', 'phone': '555-2718', 'email': 'lj@gmail.net'},
-         {'name': 'Ruslan', 'phone': '422-145-9098', 'email': 'rus-lan.cha@yandex.ru'},
-         {'name': 'John', 'phone': '233-421-32', 'email': ''},
-         {'name': 'Lara', 'phone': '+7998-676-2532', 'email': 'g.lara89@gmail.com'},
-         {'name': 'Alina', 'phone': '+7948-799-2434'},
-         {'name': 'Robert', 'phone': '420-2011', 'email': ''},
-         {'name': 'Riyad', 'phone': '128-8890-128', 'email': 'r.mahrez@mail.net'},
-         {'name': 'Khabib', 'phone': '+7995-600-9080', 'email': 'kh.nurmag@gmail.com'},
-         {'name': 'Olga', 'phone': '6449-314-1213', 'email': ''},
-         {'name': 'Roman', 'phone': '+7459-145-8059'},
-         {'name': 'Maria', 'phone': '12-129-3148', 'email': 'm.sharapova@gmail.com'},
-         {'name': 'Fedor', 'phone': '+7445-341-0545', 'email': ''},
-         {'name': 'Tim', 'phone': '242-449-3141', 'email': 'timm.ggg@yandex.ru'}]
+# users = [{'name': 'Todd', 'phone': '551-1414', 'email': 'todd@gmail.com'},
+#          {'name': 'Helga', 'phone': '555-1618'},
+#          {'name': 'Olivia', 'phone': '449-3141', 'email': ''},
+#          {'name': 'LJ', 'phone': '555-2718', 'email': 'lj@gmail.net'},
+#          {'name': 'Ruslan', 'phone': '422-145-9098', 'email': 'rus-lan.cha@yandex.ru'},
+#          {'name': 'John', 'phone': '233-421-32', 'email': ''},
+#          {'name': 'Lara', 'phone': '+7998-676-2532', 'email': 'g.lara89@gmail.com'},
+#          {'name': 'Alina', 'phone': '+7948-799-2434'},
+#          {'name': 'Robert', 'phone': '420-2011', 'email': ''},
+#          {'name': 'Riyad', 'phone': '128-8890-128', 'email': 'r.mahrez@mail.net'},
+#          {'name': 'Khabib', 'phone': '+7995-600-9080', 'email': 'kh.nurmag@gmail.com'},
+#          {'name': 'Olga', 'phone': '6449-314-1213', 'email': ''},
+#          {'name': 'Roman', 'phone': '+7459-145-8059'},
+#          {'name': 'Maria', 'phone': '12-129-3148', 'email': 'm.sharapova@gmail.com'},
+#          {'name': 'Fedor', 'phone': '+7445-341-0545', 'email': ''},
+#          {'name': 'Tim', 'phone': '242-449-3141', 'email': 'timm.ggg@yandex.ru'}]
+#
+#
+# mylist = []
+# for book in users:
+#     if 'email' in book and book['email'] != '':
+#         continue
+#     else:
+#         mylist.append(book['name'])
+# print(*sorted(mylist))
+
+
+# d = {
+#     "0": "zero",
+#     "1": "one",
+#     "2": "two",
+#     "3": "three",
+#     "4": "four",
+#     "5": "five",
+#     "6": "six",
+#     "7": "seven",
+#     "8": "eight",
+#     "9": "nine"
+# }
+# n = input()
+#
+# for k in n:
+#     print(d[k], end=' ')
+
+# print(*[d[key] for key in input()])
+
+
+# course_dict = {
+#     'CS101': '3004, Хайнс, 8:00',
+#     'CS102': '4501, Альварадо, 9:00',
+#     'CS103': '6755, Рич, 10:00',
+#     'NT110': '1244, Берк, 11:00',
+#     'CM241': '1411, Ли, 13:00'
+# }
+# key = input()
+# print(f'{key}: {course_dict[key]}')
+
+# text = input().lower().replace('"', '')
+#
+# digits = {
+#     ".": '1', ",": '11', "?": '111', "!": '1111', ":": '11111',
+#     "a": '2', "b": '22', "c": '222',
+#     "d": '3', "e": '33', "f": '333',
+#     "g": '4', "h": '44', "i": '444',
+#     "j": '5', "k": '55', "l": '555',
+#     "m": '6', "n": '66', "o": '666',
+#     "p": '7', "q": '77', "r": '777', "s": '7777',
+#     "t": '8', "u": '88', "v": '888',
+#     "w": '9', "x": '99', "y": '999', "z": '9999',
+#     " ": '0'
+# }
+#
+# for i in text:
+#     print(digits[i], end='')
+
+# keyboard = {
+#     "1": ".,?!:",
+#     "2": "ABC",
+#     "3": "DEF",
+#     "4": "GHI",
+#     "5": "JKL",
+#     "6": "MNO",
+#     "7": "PQRS",
+#     "8": "TUV",
+#     "9": "WXYZ",
+#     "0": " "
+# }
+#
+# for letter in input().upper():
+#     for key, value in keyboard.items():
+#         if letter in value:
+#             print(key * (value.index(letter) + 1), end="")
+
+
+
+
 
