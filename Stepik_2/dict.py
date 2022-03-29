6a88ac7780ffb15b661f6d99ddbd4c4c1ce3eb5c
brodvey77 <s264157250r@gmail.com>
2022-03-29 16:09:52 +0300

    290322

--- Stepik_2/dict.py
+++ Stepik_2/dict.py
@@ -213,4 +213,116 @@
 # for k in text.upper():
 #     if k in letters:
 #         print(my_dict[k], end=' ')
+#
+# numbers = [9, 8, 32, 1, 10, 1, 10, 23, 1, 4, 10, 4, 2, 2, 2, 2, 1, 10, 1, 2, 2, 32, 23, 23]
+#
+# result = {}
+# for num in numbers:
+#     if num not in result:
+#         result[num] = 1
+#     else:
+#         result[num] += 1
 
+# numbers = [9, 8, 32, 1, 10, 1, 10, 23, 1, 4, 10, 4, 2, 2, 2, 2, 1, 10, 1, 2, 2, 32, 23, 23]
+#
+# result = {}
+# for num in numbers:
+#     result[num] = result.get(num, 0) + 1
+
+# num = int(input())
+#
+# description = {1: 'One', 2: 'Two', 3: 'Three'}
+#
+# print(description.get(num, 'Unknown'))
+
+
+# dct = {'понедельник': 1, 'вторник': 2, 'среда': 3}
+#
+# print(dct.get('понедельник', 'Не найдено'))
+
+# dct = {'понедельник': 1, 'вторник': 2, 'среда': 3}
+#
+# print(dct.get('пятница', 'Не найдено'))
+
+
+# result = {}
+# for i in range(1, 16):
+#     result[i] = result.get(i, i**2)
+#
+# print(result)
+
+
+# result = {i: i**2 for i in range(1, 16)}
+
+# dict1 = {'a': 100, 'z': 333, 'b': 200, 'c': 300, 'd': 45, 'e': 98, 't': 76, 'q': 34, 'f': 90, 'm': 230}
+# dict2 = {'a': 300, 'b': 200, 'd': 400, 't': 777, 'c': 12, 'p': 123, 'w': 111, 'z': 666}
+#
+# result = dict1.copy()
+#
+# for k, v in dict2.items():
+#     result[k] = result.get(k, 0) + v
+
+# text = 'footballcyberpunkextraterritorialityconversationalistblockophthalmoscopicinterdependencemamauserfff'
+#
+# result = {}
+# for i in text:
+#     result[i] = result.get(i, 0) + 1
+#
+# print(result)
+
+# {value:key for key, value in dict.items()} меняет местами ключ и значение в словаре.
+
+# s = 'orange strawberry barley gooseberry apple apricot barley currant orange melon pomegranate banana banana orange ' \
+#     'barley apricot plum grapefruit banana quince strawberry barley grapefruit banana grapes melon strawberry ' \
+#     'apricot currant currant gooseberry raspberry apricot currant orange lime quince grapefruit barley banana ' \
+#     'melon pomegranate barley banana orange barley apricot plum banana quince lime grapefruit strawberry gooseberry ' \
+#     'apple barley apricot currant orange melon pomegranate banana banana orange apricot barley plum banana grapefruit ' \
+#     'banana quince currant orange melon pomegranate barley plum banana quince barley lime grapefruit pomegranate barley'
+
+# result = {}
+#
+# for i in s.split():
+#     result[i] = result.get(i, 0) + 1
+# flag = max(result.values())
+#
+# for k, v in sorted(result.items()):
+#     if v == flag:
+#         print(k)
+#         break
+
+
+# Вам доступен список pets, содержащий информацию о собаках и их владельцах.  Каждый элемент списка – это кортеж вида
+# (кличка собаки, имя владельца, фамилия владельца, возраст владельца).
+#
+# Дополните приведенный код так, чтобы в переменной result хранился словарь, в котором для каждого владельца будут
+# перечислены его собаки. Ключом словаря должен быть кортеж (имя, фамилия, возраст владельца), а значением – список
+# кличек собак (сохранив исходный порядок следования).
+#
+# Примечание 1. Не забывайте: кортежи являются неизменяемыми, поэтому могут быть ключами словаря.
+#
+# Примечание 2. Обратите внимание, что у некоторых владельцев по несколько собак.
+#
+# Примечание 3. Выводить содержимое словаря result не нужно.
+
+# sample: {('Josh', 'King', 25): ['Rusty', 'Balto', 'Barry', 'Lassie']}
+
+pets = [('Hatiko', 'Parker', 'Wilson', 50),
+        ('Rusty', 'Josh', 'King', 25),
+        ('Fido', 'John', 'Smith', 28),
+        ('Butch', 'Jake', 'Smirnoff', 18),
+        ('Odi', 'Emma', 'Wright', 18),
+        ('Balto', 'Josh', 'King', 25),
+        ('Barry', 'Josh', 'King', 25),
+        ('Snape', 'Hannah', 'Taylor', 40),
+        ('Horry', 'Martha', 'Robinson', 73),
+        ('Giro', 'Alex', 'Martinez', 65),
+        ('Zooma', 'Simon', 'Nevel', 32),
+        ('Lassie', 'Josh', 'King', 25),
+        ('Chase', 'Martha', 'Robinson', 73),
+        ('Ace', 'Martha', 'Williams', 38),
+        ('Rocky', 'Simon', 'Nevel', 32)]
+
+result = {}
+
+for i in range(len(pets)):
+        print(pets[i])
