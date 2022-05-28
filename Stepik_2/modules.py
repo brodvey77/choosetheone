790b58c3011d3b65338518a16e2149d49d953fab
brodvey77 <s264157250r@gmail.com>
2022-05-28 22:11:41 +0300

    add new result 280522

--- Stepik_2/modules.py
+++ Stepik_2/modules.py
@@ -284,3 +284,16 @@ import random
 #
 #
 # generate_passwords(n, m)
+
+
+n = 10**6
+k = 0
+s0 = 16
+for _ in range(n):
+    x = random.uniform(-2, 2)
+    y = random.uniform(-2, 2)
+
+    if x**3 + y**4 + 2 >= 0 and 3*x + y**2 <= 2:
+        k += 1
+
+print((k/n)*s0)
