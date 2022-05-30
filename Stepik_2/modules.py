d21a5b931b8545d1f18164cac1f1a2a2c558c437
iamnigan <brodvey77@aol.com>
2022-05-30 11:02:30 +0300

    300522_1

--- Stepik_2/modules.py
+++ Stepik_2/modules.py
@@ -286,14 +286,27 @@ import random
 # generate_passwords(n, m)
 
 
-n = 10**6
-k = 0
-s0 = 16
-for _ in range(n):
-    x = random.uniform(-2, 2)
-    y = random.uniform(-2, 2)
+# n = 10**6
+# k = 0
+# s0 = 16
+# for _ in range(n):
+#     x = random.uniform(-2, 2)
+#     y = random.uniform(-2, 2)
+#
+#     if x**3 + y**4 + 2 >= 0 and 3*x + y**2 <= 2:
+#         k += 1
+#
+# print((k/n)*s0)
 
-    if x**3 + y**4 + 2 >= 0 and 3*x + y**2 <= 2:
-        k += 1
-
-print((k/n)*s0)
+# import random
+# n = 10**6
+# k = 0
+# s0 = 4
+# for _ in range(n):
+#     x = random.uniform(-1, 1)
+#     y = random.uniform(-1, 1)
+#
+#     if x**2 + y**2 <= 1:
+#         k += 1
+#
+# print((k/n)*s0)
