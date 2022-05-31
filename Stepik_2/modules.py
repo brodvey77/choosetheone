03e7f54867429c4b1ff59dc69667065ff01791a4
iamnigan <brodvey77@aol.com>
2022-05-31 13:57:58 +0300

    solution 13_2_10

--- Stepik_2/modules.py
+++ Stepik_2/modules.py
@@ -452,10 +452,18 @@ import random
 # print(F(n)/F(m))
 
 
-from fractions import Fraction as F
-n, m = input(), input()
+# from fractions import Fraction as F
+# n, m = input(), input()
+#
+# print(f'{n} + {m} = {F(n) + F(m)}')
+# print(f'{n} - {m} = {F(n) - F(m)}')
+# print(f'{n} * {m} = {F(n) * F(m)}')
+# print(f'{n} / {m} = {F(n) / F(m)}')
 
-print(f'{n} + {m} = {F(n) + F(m)}')
-print(f'{n} - {m} = {F(n) - F(m)}')
-print(f'{n} * {m} = {F(n) * F(m)}')
-print(f'{n} / {m} = {F(n) / F(m)}')
+# from fractions import Fraction as F
+# n = int(input())
+# lst = [F(1, i)**2 for i in range(1, n + 1)]
+#
+# print(sum(lst))
+
+
