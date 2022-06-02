88324cdac3e2fda88aed644421865768887d941d
iamnigan <brodvey77@aol.com>
2022-06-02 17:09:27 +0300

    solution 13_3_13

--- Stepik_2/modules.py
+++ Stepik_2/modules.py
@@ -509,3 +509,19 @@ import random
 # a = set(nums)
 #
 # print(*sorted(a), sep='\n')
+
+# a, b = complex(input()), complex(input())
+# print(f'{a} + {b} = {a + b}')
+# print(f'{a} - {b} = {a - b}')
+# print(f'{a} * {b} = {a * b}')
+
+# numbers = [3 + 4j, 3 + 1j, -7 + 3j, 4 + 8j, -8 + 10j, -3 + 2j, 3 - 2j, -9 + 9j, -1 - 1j, -1 - 10j, -20 + 15j,
+#            -21 + 1j, 1j, -3 + 8j, 4 - 6j, 8 + 2j, 2 + 3j]
+# modules = {}
+#
+# for i in numbers:
+#     modules[abs(i)] = i
+# print(modules[max(modules)], max(modules), sep='\n')
+
+
+
