d703850afc725c56b818ccb8c73a66704ea6c049
iamnigan <brodvey77@aol.com>
2022-09-12 16:12:27 +0300

    heart

--- Stepik_2/modules.py
+++ Stepik_2/modules.py
@@ -1719,3 +1719,21 @@ import turtle
 #
 # turtle.hideturtle()
 # turtle.done()
+
+
+# heart
+
+# import turtle
+# from math import cos,sin,pi
+# turtle.hideturtle()
+# a = 2*pi
+# t = 0
+# turtle.fillcolor("red")
+#
+# turtle.begin_fill()
+# while t < a:
+#   x = 128 * sin(t)**3
+#   y = 8 * (13*cos(t)-5*cos(2*t)-2*cos(3*t)-cos(4*t)-5)
+#   turtle.goto(x,y)
+#   t += 0.2
+# turtle.end_fill()
