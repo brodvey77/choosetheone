f4e42a96d6f757e01e614ee14f26099baaf8f6db
Nigan <brodvey77@gmail.com>
2022-09-14 11:27:23 +0300

    15.2_9

--- Stepik_2/functions_2.py
+++ Stepik_2/functions_2.py
@@ -4,3 +4,19 @@
 #     return [[value for i in range(m)] for j in range(n)]
 
 
+# def f(n=3):
+#     return n + 7
+#
+#
+# print(f(f(f())))
+
+# def func(*args):
+#     return max(args) + min(args)
+#
+#
+# print(func(10, 15, *[31, 42, 5, 1], *(17, 28, 19, 100), 13, 12))
+
+# def count_args(*args):
+#     return len(args)
+#
+# print(count_args([], (''), 'a', 12, False))
