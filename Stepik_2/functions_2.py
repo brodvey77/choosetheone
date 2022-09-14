6d5c73e4ca59d644d47cf0c9d6319367bf897160
Nigan <brodvey77@gmail.com>
2022-09-14 12:37:08 +0300

    15.2_11

--- Stepik_2/functions_2.py
+++ Stepik_2/functions_2.py
@@ -26,3 +26,14 @@ import math
 #     return sum([i**2 for i in args])
 #
 # print(sq_sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+
+def mean(*args):
+    l = [i for i in args if type(i) == int or type(i) == float]
+    if len(l) == 0:
+        return 0.0
+    else:
+        return sum(l)/len(l)
+
+
+
+print(mean())
