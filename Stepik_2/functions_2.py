b5abc46dfa12effd922341379003422e4d055685
Nigan <brodvey77@gmail.com>
2022-09-15 11:48:12 +0300

    15.2_15

--- Stepik_2/functions_2.py
+++ Stepik_2/functions_2.py
@@ -63,3 +63,11 @@ import math
 #
 #
 # print_products([4], {}, 1, 2, {'Beegeek'}, '')
+
+
+# def info_kwargs(**kwargs):
+#     for k, v in sorted(kwargs.items()):
+#         print(f'{k}: {v}')
+#
+#
+# info_kwargs(first_name='Timur', last_name='Guev', age=28, job='teacher')
