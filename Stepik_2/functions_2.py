d36f84c3ac4ca39f8a148d435da7800a0e397b3f
Nigan <brodvey77@gmail.com>
2022-09-15 14:35:08 +0300

    examples

--- Stepik_2/functions_2.py
+++ Stepik_2/functions_2.py
@@ -71,3 +71,68 @@ import math
 #
 #
 # info_kwargs(first_name='Timur', last_name='Guev', age=28, job='teacher')
+
+
+# commands = {'start': start, 'stop': stop, 'pause': pause}  # словарь соответствия команда → функция
+#
+# command = input()        # считываем название команды
+#
+# commands[command]()      # вызываем нужную функцию через словарь по ключу
+
+
+# s1 = 'python'
+# s2 = 'stepicon'
+# s3 = 'beegeek'
+#
+# print(min(s1, s2, s3, key=len))
+# print(max(s1, s2, s3, key=len))
+
+
+# def f(x):
+#     return x**2
+#
+#
+# def g(x):
+#     return x**3
+#
+#
+# funcs = [f, g]
+# print(funcs[0](5), funcs[1](5))
+
+
+# def comparator(pair):
+#     return pair[1]
+#
+#
+# pairs = [(5, 4), (3, 2), (1, 7), (8, 2)]
+# pairs.sort(key=comparator)
+# print(pairs)
+
+# def comparator(pair):
+#     return pair[0] + pair[1]
+#
+#
+# pairs = [(5, 4), (3, 2), (1, 7), (8, 2)]
+# pairs.sort(key=comparator, reverse=True)
+# print(pairs)
+
+# def f1(x):
+#     return 2*x+1
+#
+#
+# def f2(x):
+#     return x**2
+#
+#
+# def f3(x):
+#     return -x**2+1
+#
+#
+# def f4(x):
+#     return x-3
+#
+#
+# funcs = [f1, f2, f3, f4]
+# i = int(input())
+# print(funcs[i](2))
+
