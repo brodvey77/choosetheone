f1b586ab4b846e083a854fab749271836d436486
Nigan <brodvey77@gmail.com>
2022-09-16 12:46:41 +0300

    fix star turtle

--- Stepik_2/modules.py
+++ Stepik_2/modules.py
@@ -1768,11 +1768,12 @@ import turtle
 
 
 import turtle
-turtle.speed(1)
-turtle.hideturtle
+turtle.speed(0)
+turtle.penup()
+turtle.hideturtle()
 turtle.fillcolor('red')
-turtle.begin_fill
+turtle.begin_fill()
 for i in range(5):
     turtle.forward(200)
     turtle.right(144)
-turtle.end_fill
+turtle.end_fill()
