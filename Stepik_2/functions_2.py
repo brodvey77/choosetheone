8469bc645884f05ac377964b94ac8dacf81fbd4a
Nigan <brodvey77@gmail.com>
2022-09-19 14:51:39 +0300

    15.4_12

--- Stepik_2/functions_2.py
+++ Stepik_2/functions_2.py
@@ -171,6 +171,16 @@ import math
 # numbers.sort(key=sum_min_max)
 # print(numbers)
 
+athletes = [('Дима', 10, 130, 35), ('Тимур', 11, 135, 39), ('Руслан', 9, 140, 33), ('Рустам', 10, 128, 30),
+            ('Амир', 16, 170, 70), ('Рома', 16, 188, 100), ('Матвей', 17, 168, 68), ('Петя', 15, 190, 90)]
 
 
+def choice_number(col):
+    return col[n - 1]
 
+
+n = int(input())
+
+names = sorted(athletes, key=choice_number)
+for i in names:
+    print(*i)
