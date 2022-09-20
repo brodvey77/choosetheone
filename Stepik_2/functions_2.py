939ecb35c4dd0d5d03a630bfb853b5b050d1df55
brodv <s264157250r@gmail.com>
2022-09-20 17:59:23 +0300

    15.5

--- Stepik_2/functions_2.py
+++ Stepik_2/functions_2.py
@@ -253,5 +253,17 @@ import math
 # print(*sorted(numbers, key=comparator))
     
 
+def filter(function, items):
+    result = []
+    for item in items:
+        if function(item):
+            result.append(item)
+    return result
 
-        
+def predicate(word):
+    return word == word[::-1]
+
+
+words = ['abba', 'qwerty', 'python', 'a', 'deed', 'nun', 'level', 'language', 'deified', 'bbbbb', 'mother', 'sister', 'surface', '1234321']
+filtered = filter(predicate, words)
+print(len(filtered))
