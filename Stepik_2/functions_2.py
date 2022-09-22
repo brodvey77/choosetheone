5a4915536950a08f474cb6583b2fe5f4c3fa88a7
brodv <s264157250r@gmail.com>
2022-09-22 11:23:30 +0300

    sum list to list

--- Stepik_2/functions_2.py
+++ Stepik_2/functions_2.py
@@ -427,12 +427,22 @@
 # print(list1 == list2)
 
 
-def filter_vowels(letter):
-    return letter in 'aeiou'
+# def filter_vowels(letter):
+#     return letter in 'aeiou'
 
 
-letters = ['a', 'b', 'd', 'e', 'i', 'j', 'o']
+# letters = ['a', 'b', 'd', 'e', 'i', 'j', 'o']
 
-filtered_vowels = filter(filter_vowels, letters)
+# filtered_vowels = filter(filter_vowels, letters)
 
-print(*filtered_vowels)
+# print(*filtered_vowels)
+
+# random_list = [1, 'a', 0, False, True, '0', 7, '']
+# filtered_list = list(filter(None, random_list))
+# print(filtered_list)
+
+listA = [2, 3, 4]
+listB = [3, 2, 1]
+
+result = sum(map(pow, listA, listB))
+print(result)
