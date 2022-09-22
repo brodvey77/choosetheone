e21e46f50b248853d67d51765165de4950c4556c
brodv <s264157250r@gmail.com>
2022-09-22 12:52:25 +0300

    examples

--- Stepik_2/functions_2.py
+++ Stepik_2/functions_2.py
@@ -441,8 +441,28 @@
 # filtered_list = list(filter(None, random_list))
 # print(filtered_list)
 
-listA = [2, 3, 4]
-listB = [3, 2, 1]
+# listA = [2, 3, 4]
+# listB = [3, 2, 1]
 
-result = sum(map(pow, listA, listB))
+# result = sum(map(pow, listA, listB))
+# # print(result)
+# from operator import mul
+# from functools import reduce
+
+# result = reduce(mul, range(1, 6))
+# print(result)
+# from operator import add
+
+# result = list(map(add, 'abc', '1234'))
+# print(result)
+
+# from operator import mul
+
+# result = list(map(mul, ['a', 'b', 'c'], [1, 2, 3]))
+# print(result)
+
+from operator import add
+from functools import reduce
+
+result = reduce(add, [[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 print(result)
