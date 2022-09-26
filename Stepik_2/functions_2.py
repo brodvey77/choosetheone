5ca4cb77e60294c18949edd9487ae4fc083ecf80
Nigan <brodvey77@gmail.com>
2022-09-26 16:58:33 +0300

    start 15.8_7

--- Stepik_2/functions_2.py
+++ Stepik_2/functions_2.py
@@ -567,14 +567,14 @@
 # print(func('bcdA'))
 # print(func('abcdA'))
 
-# is_non_negative_num = lambda num: num.replace('.','', 1).isdigit()
-#
-#
-# print(is_non_negative_num('10.34ab'))
-# print(is_non_negative_num('10.45'))
-# print(is_non_negative_num('-18'))
-# print(is_non_negative_num('-34.67'))
-# print(is_non_negative_num('987'))
-# print(is_non_negative_num('abcd'))
-# print(is_non_negative_num('123.122.12'))
-# print(is_non_negative_num('123.122'))
+is_num = lambda num: num.replace('.','', 1)
+
+print(is_num('10.34ab'))
+print(is_num('10.45'))
+print(is_num('1-1'))
+print(is_num('-34.67'))
+print(is_num('987'))
+print(is_num('abcd'))
+print(is_num('123.122.12'))
+print(is_num('-123.122'))
+print(is_num('--13.2'))
