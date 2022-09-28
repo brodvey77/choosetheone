d77f4adf16342af61add39fa09f2a6fc50dac5e9
Nigan <brodvey77@gmail.com>
2022-09-28 10:42:44 +0300

    start 15.8_9

--- Stepik_2/functions_2.py
+++ Stepik_2/functions_2.py
@@ -580,3 +580,14 @@
 # print(is_num('--13.2'))
 # print(is_num('.-95.'))
 # print(is_num('1-1'))
+
+
+# words = ['beverage', 'monday', 'abroad', 'bias', 'abuse', 'abolish', 'abuse', 'abuse', 'bid', 'wednesday', 'able',
+#          'betray', 'accident', 'abduct', 'bigot', 'bet', 'abandon', 'besides', 'access', 'friday', 'bestow', 'abound',
+#          'absent', 'beware', 'abundant', 'abnormal', 'aboard', 'about', 'accelerate', 'abort', 'thursday', 'tuesday',
+#          'sunday', 'berth', 'beyond', 'benevolent', 'abate', 'abide', 'bicycle', 'beside', 'accept', 'berry',
+#          'bewilder', 'abrupt', 'saturday', 'accessory', 'absorb']
+#
+# a = list(filter(lambda x: len(x) == 6, words))
+#
+# print(*sorted(a))
