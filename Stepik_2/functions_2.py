7730a53793f284c8437ea86c5b75353681a8b556
Nigan <brodvey77@gmail.com>
2022-09-29 12:31:49 +0300

    start 15.8_11

--- Stepik_2/functions_2.py
+++ Stepik_2/functions_2.py
@@ -2,7 +2,7 @@
 #     if m == None:
 #         m = n
 #     return [[value for i in range(m)] for j in range(n)]
-
+from functools import reduce
 
 # def f(n=3):
 #     return n + 7
@@ -592,13 +592,25 @@
 #
 # print(*sorted(a))
 
-numbers = [46, 61, 34, 17, 56, 26, 93, 1, 3, 82, 71, 37, 80, 27, 77, 94, 34, 100, 36, 81, 33, 81, 66, 83, 41, 80, 80,
-           93, 40, 34, 32, 16, 5, 16, 40, 93, 36, 65, 8, 19, 8, 75, 66, 21, 72, 32, 41, 59, 35, 64, 49, 78, 83, 27,
-           57, 53, 43, 35, 48, 17, 19, 40, 90, 57, 77, 56, 80, 95, 90, 27, 26, 6, 4, 23, 52, 39, 63, 74, 15, 66, 29,
-           88, 94, 37, 44, 2, 38, 36, 32, 49, 5, 33, 60, 94, 89, 8, 36, 94, 46, 33]
+# numbers = [46, 61, 34, 17, 56, 26, 93, 1, 3, 82, 71, 37, 80, 27, 77, 94, 34, 100, 36, 81, 33, 81, 66, 83, 41, 80, 80,
+#            93, 40, 34, 32, 16, 5, 16, 40, 93, 36, 65, 8, 19, 8, 75, 66, 21, 72, 32, 41, 59, 35, 64, 49, 78, 83, 27,
+#            57, 53, 43, 35, 48, 17, 19, 40, 90, 57, 77, 56, 80, 95, 90, 27, 26, 6, 4, 23, 52, 39, 63, 74, 15, 66, 29,
+#            88, 94, 37, 44, 2, 38, 36, 32, 49, 5, 33, 60, 94, 89, 8, 36, 94, 46, 33]
 #
 # numbers = list(filter(lambda x: not(x%2 and x > 47), numbers))
 # b = list(map(lambda num: num // 2 if num % 2 == 0 else num, numbers))
 # print(*b)
 
 # print(*map(lambda x: [x // 2, x][x % 2], filter(lambda x: x < 48 or not x % 2, numbers)))
+
+# data = [(19542209, 'New York'), (4887871, 'Alabama'), (1420491, 'Hawaii'), (626299, 'Vermont'),
+#         (1805832, 'West Virginia'), (39865590, 'California'), (11799448, 'Ohio'), (10711908, 'Georgia'),
+#         (10077331, 'Michigan'), (10439388, 'Virginia'), (7705281, 'Washington'), (7151502, 'Arizona'),
+#         (7029917, 'Massachusetts'), (6910840, 'Tennessee')]
+
+# a = sorted(data, key=lambda x: x[1][-1], reverse=True)
+# for i in a:
+#         print(i[1] + ':', i[0])
+#
+# for pop, city in sorted(data, key=lambda x: x[1][-1], reverse=True):
+#     print(f'{city}: {pop}')
