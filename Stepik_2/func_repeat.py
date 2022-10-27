de6d66ba131123bbec4fade9f48ec882c0c9fc62
Nigan <brodvey77@gmail.com>
2022-10-27 15:39:43 +0300

    repeat

--- Stepik_2/func_repeat.py
+++ Stepik_2/func_repeat.py
@@ -7,7 +7,7 @@
 # print(matrix(3))                  # матрица 3 × 3 из 0
 # print(matrix(2, 5))               # матрица 2 × 5 из 0
 # print(matrix(3, 4, 9))            # матрица 3 × 4 из 9
-from functools import reduce
+# from functools import reduce
 
 # def count_args(*args):
 #     return len(args)
@@ -244,4 +244,38 @@ from functools import reduce
 
 # print(reduce(summ_numbers, map(square_num, numbers)))
 
+#
+# numbers = [77, 293, 28, 242, 213, 285, 71, 286, 144, 276, 61, 298, 280, 214, 156, 227, 228, 51, -4, 202, 58, 99, 270,
+#            219, 94, 253, 53, 235, 9, 158, 49, 183, 166, 205, 183, 266, 180, 6, 279, 200, 208, 231, 178, 201, 260,
+#            -35, 152, 115, 79, 284, 181, 92, 286, 98, 271, 259, 258, 196, -8, 43, 2, 128, 143, 43, 297, 229, 60,
+#            254, -9, 5, 187, 220, -8, 111, 285, 5, 263, 187, 192, -9, 268, -9, 23, 71, 135, 7, -161, 65, 135, 29,
+#            148, 242, 33, 35, 211, 5, 161, 46, 159, 23, 169, 23, 172, 184, -7, 228, 129, 274, 73, 197, 272, 54, 278,
+#            26, 280, 13, 171, 2, 79, -2, 183, 10, 236, 276, 4, 29, -10, 41, 269, 94, 279, 129, 39, 92, -63, 263, 219,
+#            57, 18, 236, 291, 234, 10, 250, 0, 64, 172, 216, 30, 15, 229, 205, 123, -105]
+#
+# def two(x):
+#     if len(str(abs(x))) == 2 and x % 7 == 0:
+#         return x
+#
+# def kvadrat(x):
+#     return x**2
+#
+# print(sum(map(kvadrat, filter(two, numbers))))
 
+
+
+# def func_apply(func, args):
+#     return list(map(func, args))
+#
+#
+#
+# def add3(x):
+#     return x + 3
+#
+#
+# def mul7(x):
+#     return x * 7
+#
+# print(func_apply(mul7, [1, 2, 3, 4, 5, 6]))
+# print(func_apply(add3, [1, 2, 3, 4, 5, 6]))
+# print(func_apply(str, [1, 2, 3, 4, 5, 6]))
