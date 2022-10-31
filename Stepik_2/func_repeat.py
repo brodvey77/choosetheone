8053decc49e2df3bcd9f0ad3941601e5161749db
Nigan <brodvey77@gmail.com>
2022-10-31 09:33:39 +0300

    exam func

--- Stepik_2/func_repeat.py
+++ Stepik_2/func_repeat.py
@@ -8,7 +8,7 @@
 # print(matrix(2, 5))               # матрица 2 × 5 из 0
 # print(matrix(3, 4, 9))            # матрица 3 × 4 из 9
 # from functools import reduce
-
+import operator
 # def count_args(*args):
 #     return len(args)
 #
@@ -279,3 +279,255 @@
 # print(func_apply(mul7, [1, 2, 3, 4, 5, 6]))
 # print(func_apply(add3, [1, 2, 3, 4, 5, 6]))
 # print(func_apply(str, [1, 2, 3, 4, 5, 6]))
+
+from functools import reduce
+
+# data = [['Tokyo', 35676000, 'primary'],
+#         ['New York', 19354922, 'nan'],
+#         ['Mexico City', 19028000, 'primary'],
+#         ['Mumbai', 18978000, 'admin'],
+#         ['Sao Paulo', 18845000, 'admin'],
+#         ['Delhi', 15926000, 'admin'],
+#         ['Shanghai', 14987000, 'admin'],
+#         ['Kolkata', 14787000, 'admin'],
+#         ['Los Angeles', 12815475, 'nan'],
+#         ['Dhaka', 12797394, 'primary'],
+#         ['Buenos Aires', 12795000, 'primary'],
+#         ['Karachi', 12130000, 'admin'],
+#         ['Cairo', 11893000, 'primary'],
+#         ['Rio de Janeiro', 11748000, 'admin'],
+#         ['Osaka', 11294000, 'admin'],
+#         ['Beijing', 11106000, 'primary'],
+#         ['Manila', 11100000, 'primary'],
+#         ['Moscow', 10452000, 'primary'],
+#         ['Istanbul', 10061000, 'admin'],
+#         ['Paris', 9904000, 'primary']]
+
+# Cities: Beijing, Buenos Aires, ...
+
+# a = map(lambda x: x[0], sorted(filter(lambda x: x[1] > 10000000 and x[2] == 'primary', data), key=lambda x: x[0]))
+#
+# print('Cities: ' + reduce(lambda x, y: x + ', ' + y, a))
+
+######## EXAM ##########
+
+# To: <mail>
+# Приветствую, <name>!
+# Вам назначен экзамен, который пройдет <date>, в <time>.
+# По адресу: <place>.
+# Экзамен будет проводить <teacher> в кабинете <number>.
+# Желаем удачи на экзамене!
+#
+# def generate_letter(mail, name, date, time, place, teacher=' Тимур Гуев', number=17):
+#         return f'''To: {mail}\nПриветствую, {name}!\nВам назначен экзамен, который пройдет {date}, в {time}.\nПо адресу: {place}.\nЭкзамен будет проводить {teacher} в кабинете {number}.\nЖелаем удачи на экзамене!'''
+#
+#
+# print(generate_letter('lara@yandex.ru', 'Лариса', '10 декабря', '12:00', 'Часова 23, корпус 2'))
+# print()
+# print(generate_letter('lara@yandex.ru', 'Лариса', '10 декабря', '12:00',
+#                       'Часова 23, корпус 2', 'Василь Ярошевич', 23))
+
+
+
+# def pretty_print(objects, side='-', delimiter='|'):
+#     line = f'{delimiter} {(" " + delimiter + " ").join(str(item) for item in objects)} {delimiter}'
+#     print(f' {side * (len(line) - 2)}')
+#     print(line)
+#     print(f' {side * (len(line) - 2)}')
+#
+# pretty_print([1, 2, 10, 23, 123, 3000])
+# pretty_print(['abc', 'def', 'ghi', '12345'])
+# pretty_print(['abc', 'def', 'ghi'], side='*')
+# pretty_print(['abc', 'def', 'ghi'], delimiter='#')
+# pretty_print(['abc', 'def', 'ghi'], side='*', delimiter='#')
+
+
+# print((lambda x: (x + 3) * 5 / 2)(3))
+
+# data = [['p', 'y', 't', 'h', 'o', 'n'], ['s', 't', 'e', 'p', 'i', 'k']]
+# result = list(map(lambda x: '.'.join(x), data))
+# print(result[1])
+
+
+# result = list(filter(str.isalpha, ['a', '1', 'b', '2']))
+#
+# print(result)
+
+# result = list(filter(str.swapcase, ['a', '1', '', 'b', '2']))
+#
+# print(result)
+
+# files = ['duwwouy440.py', 'crocst0sse.cs', 'j9t7ga2s6x.java', 'jk4nnes4tp.py', '2spc9uqzhu.doc',
+#          'qi0ujxe0c7.png', 'z5x7l1j1d8.jpg', 'i5wtdxh366.geo', 'h53s2m2p73.py', 'ojty11f02d.sx',
+#          'jyjuwlvwb3.st', 'gv4940lf8m.txt', 'op38fy9m9x.docx', 'o02ltr8vbp.xlsx', 'la97gc4js4.html',
+#          'lcihrp8c6l.py', 'z66y7dgfo1.py', 'ckoks0849e.csv']
+#
+# result = list(filter(lambda s: s.endswith('.py'), files))
+#
+# print(len(result))
+
+# print(list(filter(None, ['', 1, 7, 'beegeek', None, False, 0])))
+
+
+# from functools import reduce
+#
+# words = ['beegeek', 'stepik', 'python', 'iq-option']
+# result = reduce(lambda a, b: a if len(a) > len(b) else b, words)
+# print(result)
+
+
+# from functools import reduce
+#
+# result = reduce(lambda s, x: s + str(x), [1, 2, 3, 4, 5], '+')
+# print(result)
+
+# from functools import reduce
+# import operator
+#
+# def flatten(data):
+#     return reduce(operator.concat, data, [])
+#
+# result = flatten([[1, 2], [3, 4], [], [5]])
+#
+# print(result)
+# from functools import reduce
+# def concat(*args, sep=' '):
+#     return reduce(lambda x, y: str(x) + sep + str(y), args)
+#
+#
+#
+#
+# print(concat('hello', 'python', 'and', 'stepik'))
+# print(concat('hello', 'python', 'and', 'stepik', sep='*'))
+# print(concat('hello', 'python', sep='()()()'))
+# print(concat('hello', sep='()'))
+# print(concat(1, 2, 3, 4, 5, 6, 7, 8, 9, sep='$$'))
+
+
+# def product_of_odds(data):   # data - список целых чисел
+#     result = 1
+#     for i in data:
+#         if i % 2 == 1:
+#             result *= i
+#     return result
+
+# def product_of_odds(data):
+#         return reduce(lambda x, y: x * y, filter(lambda x: x % 2 == 1, data), 1)
+#
+#
+# print(product_of_odds(range(1, 11)))
+
+
+# words = 'the world is mine take a look what you have started'.split()
+#
+# print(*map(lambda x: f'"{x}"', words))
+
+# x!=x[::-1]
+
+# numbers = [18, 191, 9009, 5665, 78, 77, 45, 23, 19991, 908, 8976, 6565, 5665, 10, 1000, 908, 909, 232, 45654, 786]
+# print(*filter(lambda x: x != x[::-1], map(lambda x: str(x), numbers)))
+
+# numbers = [(10, -2, 3, 4), (-13, 56), (1, 9, 2), (-1, -9, -45, 32), (-1, 5, 1), (17, 0, 1), (0, 1), (3,), (39, 12), (11, -23), (10, -100, 21, 32), (3, -8), (1, 1)]
+#
+# sorted_numbers = sorted(numbers, key=lambda x: sum(x)/len(x), reverse=True)
+#
+# print(sorted_numbers)
+
+# def mul7(x):
+#     return x * 7
+#
+#
+# def add2(x, y):
+#     return x + y
+#
+#
+# def add3(x, y, z):
+#     return x + y + z
+#
+#
+# def call(f, *args):
+#     return f(*args)
+#
+#
+# print(call(mul7, 10))
+# print(call(add2, 2, 7))
+# print(call(add3, 10, 30, 40))
+# print(call(bool, 0))
+
+
+
+# def add3(x):
+#     return x + 3
+#
+#
+# def mul7(x):
+#     return x * 7
+#
+#
+# def compose(f, g):
+#     return lambda x: f(g(x))
+#
+#
+#
+# print(compose(mul7, add3)(1))
+# print(compose(add3, mul7)(2))
+# print(compose(mul7, str)(3))
+# print(compose(str, mul7)(5))
+
+# def arithmetic_operation(op):
+#         if op == '+':
+#                 return lambda x, y: x + y
+#         elif op == '-':
+#                 return lambda x, y: x - y
+#         elif op == '*':
+#                 return lambda x, y: x * y
+#         elif op == '/':
+#                 return lambda x, y: x / y
+#         else:
+#                 return lambda x, y: print('bad operation!')
+
+
+# from operator import *
+#
+# def arithmetic_operation(operation):
+#     oper = {
+#         '+': add,
+#         '-': sub,
+#         '*': mul,
+#         '/': truediv
+#     }
+#     return oper[operation]
+#
+# add = arithmetic_operation('+')
+# div = arithmetic_operation('/')
+
+# print(add(10, 20))
+# print(div(20, 5))
+
+from functools import reduce
+# text = 'cate Frog cat FROGs bee CATERS mouse cATwalk dolphin mOus Cats CatAlo'
+#
+# print(*sorted(input().split(), key=lambda x: x.lower()))
+
+# n = int(input())
+
+# def preobrazovanie(spisok):
+#     return sorted(map(lambda a: int(a), map(lambda b: b.replace('.', ''), spisok)))
+#
+# l = [input() for i in range(int(input()))]
+#
+#
+# print(preobrazovanie(l))
+
+
+# ip_adresses = [input() for _ in range(int(input()))]
+#
+# low = lambda ip: list(map(int, ip.split('.')))
+# print(*sorted(ip_adresses, key=low), sep='\n')
+
+
+import ipaddress
+
+# print(*sorted(l, key=ipaddress.IPv4Address))
+
+
