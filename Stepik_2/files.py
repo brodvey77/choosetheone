c6e14fa3afbaa5d6d5887c4b9a8e3dc64a47fcd2
Nigan <brodvey77@gmail.com>
2022-10-31 17:26:35 +0300

    add file learning

--- Stepik_2/files.py
+++ Stepik_2/files.py
@@ -119,3 +119,7 @@ from functools import reduce
 # print('Файл закрыт')
 
 
+# with open('test.txt', encoding='utf-8') as file:
+#     print('Repeat after me:', file.readline().strip())
+#     for line in file:
+#         print(line.strip() + '!')
