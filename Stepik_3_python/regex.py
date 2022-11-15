5cc84eb20c1674525486010e90ec4145b8bf96f9
Nigan <brodvey77@gmail.com>
2022-11-15 12:02:35 +0300

    regex

--- Stepik_3_python/regex.py
+++ Stepik_3_python/regex.py
@@ -13,7 +13,13 @@
 #
 # print(regex)
 # import re
-# s = 'car cat cab'
-# pattern1 = re.compile('car|cat|cab')
+# s = 'Order 1: B938274, Order 2: T8372, Order 3: g883929'
+# pattern1 = re.compile('[A-Za-z]\d{4}')
 # for i in pattern1.findall(s):
 #     print(i)
+
+import re
+s = 'Order 1: B938274, Order 2: T8372, Order 3: g883929'
+pattern1 = re.compile('[A-Za-z]\d{4}')
+for i in pattern1.findall(s):
+    print(i)
