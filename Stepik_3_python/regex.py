d3e3f2dc1926a7e2e5af122c31cb0c833662e050
Nigan <brodvey77@gmail.com>
2022-11-24 16:17:01 +0300

    regex

--- Stepik_3_python/regex.py
+++ Stepik_3_python/regex.py
@@ -60,3 +60,118 @@
 # pattern1 = re.compile('[0-1]\d:[0-5]\d|2[0-3]:[0-5]\d')
 # for i in pattern1.findall(s):
 #     print(i)
+
+# from re import search
+#
+# match1 = search(r'\w+', 'Hello, Timur')
+#
+# print(match1.group() if match1 else None)
+
+# from re import match
+#
+# match1 = match(r'[Hh]\w+', 'hello, Timur')
+# match2 = match(r'[Tt]\w+', 'hello, Timur')
+#
+# print(match1.group() if match1 else None)
+# print(match2.group() if match2 else None)
+
+
+# from re import fullmatch
+#
+# match1 = fullmatch(r'[Hh]\w+ [Tt]\w+', 'Hello, Timur')
+# match2 = fullmatch(r'[Hh]\w+, [Tt]\w+', 'Hello, Timur')
+#
+# print(match1.group() if match1 else None)
+# print(match2.group() if match2 else None)
+
+
+# from re import fullmatch
+#
+# match1 = fullmatch(r'(-)?(\d+)', '2077')
+# match2 = fullmatch(r'(-)?(\d+)', '-1337')
+#
+# print(match1.group(1))
+# print(match1.group(2))
+# print(match2.group(1, 2))
+
+# import sys
+#
+# for line in sys.stdin:
+#     print(line.strip('\n'))
+
+# import sys
+#
+# print('Python')
+#
+# sys.stdout = open('file.txt', 'w')
+#
+# print('is')
+# print('Power')
+
+# import sys
+#
+# for line in sys.stdin:
+#     print(line[::-1].strip('\n'))
+
+# import sys
+# from re import search
+#
+#
+# # text = sys.stdin.readline().strip('\n')
+# #
+# match_1 = search(r'(?P<Country>\w+),(?P<City>\w+),(?P<number>\w+)', 'asd,asdasd,asdasd')
+#
+# print(match_1.groupdict())
+
+# import sys
+# from re import fullmatch
+#
+# flag = ' '
+# for text in sys.stdin:
+#     text = text.strip('\n')
+#     if ' ' not in text:
+#         flag = '-'
+#
+#     regex = r'\d{1,3}-\d{1,3}-\d{4,10}|\d{1,3} \d{1,3} \d{4,10}'
+#
+#     match_1 = fullmatch(regex, text)
+#     # print(match_1.group())
+#     if match_1:
+#         text = text.split(flag)
+#         print(f'Код страны: {text[0]} Код города: {text[1]}, Номер: {text[2]}')
+#     else:
+#         print('нет совпадения')
+# flag = ' '
+
+
+# from re import match
+# from sys import stdin
+#
+# regex = r'(?P<A>\d+)[ -](?P<B>\d+)[ -](?P<C>\d+)'
+#
+# for line in stdin:
+#     a, b, c = match(regex, line).groupdict().values()
+#     print(f'Код страны: {a}, Код города: {b}, Номер: {c}')
+
+# import sys
+# from re import search, fullmatch
+#
+# for line in sys.stdin:
+#     match = search("(\d{1,3})[ -](\d{1,3})[ -](\d{4,10})", line)
+#     print(f"Код страны: {match.group(1)}, Код города: {match.group(2)}, Номер: {match.group(3)}")
+
+
+
+# import sys
+# from re import search
+#
+# for line in sys.stdin:
+#     match = search('(\d{1,3})[ -](\d{1,3})[ -](\d{4,10})', line)
+#     print(f'Код страны: {match.group(1)}, Код города: {match.group(2)}, Номер: {match.group(3)}')
+
+# import sys
+# from re import fullmatch
+#
+# for line in sys.stdin:
+#     match = fullmatch('_\d+[a-zA-Z]*_?', line.strip('\n'))
+#     print(bool(match))
