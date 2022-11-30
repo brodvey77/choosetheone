6b8773e6741354a30587f565eb96df42d12ad02e
Nigan <brodvey77@gmail.com>
2022-11-30 15:14:36 +0300

    regex

--- Stepik_3_python/regex.py
+++ Stepik_3_python/regex.py
@@ -273,3 +273,80 @@
 #
 # for line in sys.stdin:
 #     match = re.search('^Здравствуйте.*', line)
+
+# from re import findall, finditer
+#
+# result1 = findall(r'\w', 'beegeek')
+# result2 = finditer(r'\w', 'beegeek')
+#
+# match1, *_ = result1
+# match2, *_ = result2
+#
+# print(type(match1))
+# print(type(match2))
+
+# from re import findall
+#
+# result = findall(r'[Bb]\w+', 'But that bee can only bite once')
+#
+# print(result)
+
+#
+# from re import findall
+#
+# result = findall(r'__(\w+)__', 'They are surrounded by double underscores: __init__ or __add__')
+#
+# print(result)
+
+
+# from re import findall
+#
+# result = findall(r'(\w+)-(\w+)', 'ex-wife, self-made, twenty-six')
+#
+# print(result)
+
+
+# from re import finditer
+#
+# result = finditer(r'[Pp]\w+', 'Purse men of genuine python skin (the dorsal part)')
+#
+# for match_obj in result:
+#     print(match_obj.group())
+
+
+# from re import finditer
+#
+# result = finditer(r'A(\w+)', 'Timur, Arthur, Dima, Anri, Ruslan')
+#
+# for match_obj in result:
+#     print(match_obj.group())
+
+
+# from re import finditer
+#
+# result = finditer(r'(\d\d):(\d\d)', 'You can call me from 10:00 to 12:00')
+#
+# for match_obj in result:
+#     print(match_obj.groups())
+
+# import re
+# article = '''Stepik (до августа 2016 года Stepic) — это образовательная платформа и конструктор онлайн-курсов!
+#
+# Первые образовательные материалы были выпущены на Stepik 3 сентября 2013 года.
+# В январе 2016 года Stepik выпустил мобильные приложения под iOS и Android. В 2017 году разработаны мобильные приложения для изучения ПДД в адаптивном режиме для iOS и Android...
+#
+# На октябрь 2020 года на платформе зарегистрировано 5 миллионов пользователей!
+# Stepik позволяет любому зарегистрированному пользователю создавать интерактивные обучающие уроки и онлайн-курсы, используя видео, тексты и разнообразные задачи с автоматической проверкой и моментальной обратной связью.
+#
+# Проект сотрудничает как с образовательными учреждениями, так и c индивидуальными преподавателями и авторами.
+# Stepik сегодня предлагает онлайн-курсы от образовательных организаций, а также индивидуальных авторов!
+#
+# Система автоматизированной проверки задач Stepik была использована в ряде курсов на платформе Coursera, включая курсы по биоинформатике от Калифорнийского университета в Сан-Диего и курс по анализу данных от НИУ «Высшая школа экономики»...
+#
+# Stepik также может функционировать как площадка для проведения конкурсов и олимпиад, среди проведённых мероприятий — отборочный этап Олимпиады НТИ (2016—2020) (всероссийской инженерной олимпиады школьников, в рамках программы Национальная технологическая инициатива), онлайн-этап акции Тотальный диктант в 2017 году, соревнования по информационной безопасности StepCTF-2015...'''
+#
+# result1 = re.findall(r'^stepik.*', article, re.I|re.M)
+# result2 = re.findall(r'.*\.\.\.$|.*!$', article, re.I|re.M)
+# print(len(result1), len(result2), sep='\n')
+
+
