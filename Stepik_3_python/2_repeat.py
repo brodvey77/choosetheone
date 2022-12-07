7dc8d145da89bef69cce7b126305a2d32a83de72
Nigan <brodvey77@gmail.com>
2022-12-07 09:44:55 +0300

    2_repeat

---
+++ Stepik_3_python/2_repeat.py
@@ -0,0 +1,13 @@
+
+
+# def hide_card(card_number):
+#     card_number = card_number.replace(' ', '')
+#     return '*'*8+card_number[12:]
+#
+# card = '3456 9012 5678 1234'
+#
+# print(hide_card(card))
+#
+#
+# def hide_card(card):
+#     return '*' * 12 + card.replace(' ', '')[-4:]
