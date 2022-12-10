43f0d0f35056a82eb491d72a4e4a066c23232a99
IamNigan <s264157250r@gmail.com>
2022-12-10 21:07:08 +0300

    repeat

--- Stepik_3_python/2_repeat.py
+++ Stepik_3_python/2_repeat.py
@@ -11,3 +11,17 @@
 #
 # def hide_card(card):
 #     return '*' * 12 + card.replace(' ', '')[-4:]
+
+
+
+def same_parity(numbers):
+    if numbers == []:
+        new_numbers = []
+    if numbers[0] % 2 == 0:
+        new_numbers = filter(lambda x: x % 2 == 0, numbers)
+    else:
+        new_numbers = filter(lambda x: x % 2 != 0, numbers)
+    return list(new_numbers)
+
+print(same_parity([6, 0, 67, -7, 10, -20]))
+
