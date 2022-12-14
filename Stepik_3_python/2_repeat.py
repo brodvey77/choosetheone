f88577b201c1de14e04f960aac9f5756281e9058
Fedora State <s264157250r@gmail.com>
2022-12-14 14:57:27 +0300

    repeat

--- Stepik_3_python/2_repeat.py
+++ Stepik_3_python/2_repeat.py
@@ -35,23 +35,50 @@
 
 # print_given()
 
-def convert(string):
-    c1 = 0
-    c2 = 0
-    for i in string:
-        if i in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
-            c1 += 1
-        if i in 'abcdefghijklmnopqrstuvwxyz':
-            c2 += 1
-    if c2 > c1:
-        return string.lower()
-    if c1 > c2:
-        return string.upper()
-    else:
-        return string.lower()
+# def convert(string):
+#     c1 = 0
+#     c2 = 0
+#     for i in string:
+#         if i in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+#             c1 += 1
+#         if i in 'abcdefghijklmnopqrstuvwxyz':
+#             c2 += 1
+#     if c2 > c1:
+#         return string.lower()
+#     if c1 > c2:
+#         return string.upper()
+#     else:
+#         return string.lower()
+
+
+# def convert(text):
+#     lower_count = len(list(filter(str.islower, text)))
+#     upper_count = len(list(filter(str.isupper, text)))
+#     if lower_count >= upper_count:
+#         return text.lower()
+#     return text.upper()
+
+
+# def convert(string):
+#     if sum(1 if c.isupper() else -1 for c in string if c.isalpha()) > 0:
+#         return string.upper()
+#     return string.lower()
+
+# print(convert('BEEgeek'))
+# print(convert('pyTHON'))
+# print(convert('pi31415!'))
 
 
 
-print(convert('BEEgeek'))
-print(convert('pyTHON'))
-print(convert('pi31415!'))
+def filter_anagrams(word,words):
+    fl = []
+    for i in words:
+        if sorted(list(i)) == sorted(list(word)):
+            fl.append(i)
+    return(fl)
+
+
+
+
+
+print(filter_anagrams('отсечка', ['сеточка', 'стоечка', 'тесачок', 'чесотка']))
