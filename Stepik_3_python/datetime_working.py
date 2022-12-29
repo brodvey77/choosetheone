91250ff93b104916698ca44a0322b1bfa9b20fc4
Nigan <brodvey77@gmail.com>
2022-12-29 16:00:50 +0300

    datetime

---
+++ Stepik_3_python/datetime_working.py
@@ -0,0 +1,31 @@
+# from datetime import date
+#
+# dates = [date(2000, 12, 31), date(1999, 3, 8), date(1999, 2, 22)]
+#
+# max_date = max(dates)
+# min_date = min(dates)
+#
+# print(max_date.year + min_date.day)
+
+# from datetime import date
+#
+# dates = [date(2023, 1, 1), date(2020, 7, 20), date(2021, 9, 17), date(2022, 6, 10)]
+#
+# print(*sorted(dates, key=lambda d: d.day), sep=', ')
+
+
+# # импортируем тип date из модуля datetime
+# from datetime import date
+#
+# # выводим текущую дату
+# print(date.today())
+
+
+# импортируем тип date из модуля datetime
+from ____ import ____
+
+# создаем объект, соответсвующий дате урагана
+hurricane_andrew = ____
+
+# выводим день недели
+print(hurricane_andrew.____)
