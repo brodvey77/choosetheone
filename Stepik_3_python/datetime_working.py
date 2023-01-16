90ab7c54c0629b2691bf276c6ea3b63a02c83aa0
Nigan <brodvey77@gmail.com>
2023-01-16 11:34:31 +0300

    datetime

--- Stepik_3_python/datetime_working.py
+++ Stepik_3_python/datetime_working.py
@@ -21,11 +21,26 @@
 # print(date.today())
 
 
-# импортируем тип date из модуля datetime
-from datetime import date
+# # импортируем тип date из модуля datetime
+# from datetime import date
+#
+# # создаем объект, соответсвующий дате урагана
+# hurricane_andrew = date(1992,8,24)
+#
+# # выводим день недели
+# print(hurricane_andrew.weekday())
 
-# создаем объект, соответсвующий дате урагана
-hurricane_andrew = date(1992,8,24)
 
-# выводим день недели
-print(hurricane_andrew.weekday())
+# from datetime import date
+# florida_hurricane_dates = [date(1987, 11, 15), date(1988, 3, 12), date(1976, 5, 12)]
+#
+# # счетчик для нужного количества ураганов
+# early_hurricanes = 0
+#
+# # цикл по датам в которые был ураган
+# for hurricane in florida_hurricane_dates:
+#     # если месяц урагана меньше чем июнь (порядковый номер 6)
+#     if hurricane.month < 6:
+#         early_hurricanes += 1
+#
+# print(early_hurricanes)
