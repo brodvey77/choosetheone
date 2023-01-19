752cf333a4947bada9b860af01aa4b050b67bf5e
Nigan <brodvey77@gmail.com>
2023-01-19 16:22:31 +0300

    datetime

--- Stepik_3_python/datetime_working.py
+++ Stepik_3_python/datetime_working.py
@@ -242,4 +242,18 @@ from datetime import date
 #         return False
 
 
-print(is_correct(31, 13, 2021))
+# print(is_correct(31, 13, 2021))
+
+
+from datetime import date
+
+def is_correct(day, month, year):
+    try:
+        date(year, month, day)
+        return True
+    except:
+        return False
+
+
+
+
