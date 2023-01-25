cb16598ab94cd8bfdfc5e74083eaae867bfe407e
Nigan <brodvey77@gmail.com>
2023-01-25 14:13:27 +0300

    datetime

--- Stepik_3_python/datetime_working.py
+++ Stepik_3_python/datetime_working.py
@@ -311,3 +311,11 @@ from datetime import date
 # dt = datetime.strptime(text, pattern)
 #
 # print(dt)
+
+# from datetime import datetime
+#
+# text = 'Уважаемый пациент, доктор готов принять Вас 15.07.2022 в 08:30'
+#
+# dt = datetime.strptime(text, 'Уважаемый пациент, доктор готов принять Вас %d.%m.%Y в %H:%M')
+#
+# print(dt)
