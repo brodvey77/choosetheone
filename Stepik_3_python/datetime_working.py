5232b2e4cc31e8a4a0c25044725cb0d2808af6fc
Nigan <brodvey77@gmail.com>
2023-01-27 15:48:15 +0300

    datetime

--- Stepik_3_python/datetime_working.py
+++ Stepik_3_python/datetime_working.py
@@ -446,3 +446,14 @@ from datetime import date
 #             print(v)
 
 
+def is_available_date(booked_dates, date_for_booking):
+    l = []
+    for i in booked_dates:
+
+
+
+dates = ['04.11.2021', '05.11.2021-09.11.2021']
+some_date = '01.11.2021'
+print(is_available_date(dates, some_date))
+
+
