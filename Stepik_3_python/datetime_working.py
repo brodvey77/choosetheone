0273006a7734a4ce612e57b7860cf8fc1aec7279
Nigan <brodvey77@gmail.com>
2023-02-01 16:24:46 +0300

    datetime

--- Stepik_3_python/datetime_working.py
+++ Stepik_3_python/datetime_working.py
@@ -575,3 +575,13 @@ from datetime import date
 #
 # dt = datetime(2021, 11, 4, 13, 6) + timedelta(weeks=1, hours=12)
 # print(dt.strftime('%d.%m.%Y %H:%M:%S'))
+
+
+# from datetime import date, timedelta
+#
+# today = date(2021, 11, 4)
+# birthday = date(2022, 10, 6)
+#
+# days = (birthday - today).days
+#
+# print(days)
