163dc6f09967a8e0f3b12983b69adb69a13ad4e6
Nigan <brodvey77@gmail.com>
2023-02-10 11:35:35 +0300

    datetime

--- Stepik_3_python/datetime_working.py
+++ Stepik_3_python/datetime_working.py
@@ -705,6 +705,14 @@ from datetime import datetime, timedelta
 #
 # print(result)
 
+# from datetime import date, time, timedelta, datetime
+#
+# dates = [datetime.strptime(dt, '%d.%m.%Y') for dt in input().split()]
+#
+# diffs = [abs(dates[i] - dates[i - 1]).days for i in range(1, len(dates))]
+#
+# print(diffs)
+
 
 
 
