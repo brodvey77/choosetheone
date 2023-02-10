df29096a5c91b2f7e292f5ff0f84cc2912ee9877
Nigan <brodvey77@gmail.com>
2023-02-10 17:02:09 +0300

    datetime

--- Stepik_3_python/datetime_working.py
+++ Stepik_3_python/datetime_working.py
@@ -714,5 +714,14 @@ from datetime import datetime, timedelta
 # print(diffs)
 
 
+from datetime import datetime, timedelta
+def fill_up_missing_dates(x):
+    return x
+
+
+dates = ['01.11.2021', '07.11.2021', '04.11.2021', '03.11.2021']
+
+print(fill_up_missing_dates(dates))
+
 
 
