3dfb1528a9c4c9190f8f96fef7641a08b0e94587
Nigan <brodvey77@gmail.com>
2023-03-09 12:07:36 +0300

    echo_bot_2

--- echo_bot.py
+++ echo_bot.py
@@ -40,10 +40,11 @@
 
 #Регистрация хэндлеров в диспетчере
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, ContentType
 import logging
+
 
 API_TOKEN = '1363273986:AAEHIpzsNhJYEeeiN1I3s4sAr4j86vQYVL4'
 
@@ -61,12 +62,19 @@ async def process_help_command(message: Message):
     await message.answer('Напиши мне что-нибудь и в ответ '
                          'я пришлю тебе твое сообщение')
 
+
+# Этот хэндлер будет срабатывать на отправку боту фото
+async def send_photo_echo(message: Message):
+    await message.reply_photo(message.photo[0].file_id)
+
+
 async def send_echo(message: Message):
     await message.reply(text=message.text)
 
 # Регистрируем хэндлеры
 dp.message.register(process_start_command, Command(commands=['start']))
 dp.message.register(process_help_command, Command(commands='help'))
+dp.message.register(send_photo_echo, F.photo)
 dp.message.register(send_echo)
 
 if __name__ == '__main__':
