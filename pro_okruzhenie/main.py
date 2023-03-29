b7550ab0c5e04b89f37c2c3d8803b58ed23420f8
Nigan <brodvey77@gmail.com>
2023-03-29 14:33:25 +0300

    okruzhenie

--- pro_okruzhenie/main.py
+++ pro_okruzhenie/main.py
@@ -1,9 +1,15 @@
-import os
-
-import dotenv
+from environs import Env
 
 
-dotenv.load_dotenv()
+env = Env()         # Создаем экземпляр класса ENV
+env.read_env()      # Методом read_env() читаем файл .env
+                    # и загружаем из него переменные в окружение
 
-print(os.getenv('BOT_TOKEN'))
-print(os.getenv('ADMIN_ID'))
+
+bot_token = env('BOT_TOKEN')         # Сохраняем значение переменной окружения в переменную bot_token
+admin_id = env.int('ADMIN_ID')       # Преобразуем значение переменной окружения к типу int
+                                     # и сохраняем в переменной admin_id
+
+
+print(bot_token)
+print(admin_id)
