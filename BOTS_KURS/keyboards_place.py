1ceb1fadcb82b2efa265a4e5bb7f7719f8e3aadc
Nigan <brodvey77@gmail.com>
2023-04-04 16:32:31 +0300

    keyboard

--- BOTS_KURS/keyboards_place.py
+++ BOTS_KURS/keyboards_place.py
@@ -1,37 +1,27 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart, Text
 from aiogram.types import (KeyboardButton, Message, ReplyKeyboardMarkup, ReplyKeyboardRemove)
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+
+
 
 API_TOKEN: str = '1363273986:AAEHIpzsNhJYEeeiN1I3s4sAr4j86vQYVL4'
 
 # Создаем объекты бота и диспетчера
 bot: Bot = Bot(token=API_TOKEN)
 dp: Dispatcher = Dispatcher()
+kb_builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
 
-buttons: list[KeyboardButton] = []
-keyboard: list[list[KeyboardButton]] = []
 
-# Заполняем список списками с кнопками
-for i in range(1, 1201):
-    buttons.append(KeyboardButton(text=str(i)))
-    if not i % 12:
-        keyboard.append(buttons)
-        buttons = []
+buttons_1: list[KeyboardButton] = [KeyboardButton(
+    text=f'button {i + 1}') for i in range(10)]
 
-# Создаем объект клавиатуры, добавляя в него список списков с кнопками
-my_keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
-                                        keyboard=keyboard,
-                                        resize_keyboard=True)
+kb_builder.add(*buttons_1)
+kb_builder.adjust(2, 1, repeat=True)
 
-# Этот хэндлер будет срабатывать на команду "/start"
-# и отправлять в чат клавиатуру
 @dp.message(CommandStart())
-async def process_start_command(message: Message):
-    await message.answer(text='Это тест кнопок', reply_markup=my_keyboard)
-
-
-
-
+async def start(message: Message):
+    await message.answer(text='MY KEYBOARD', reply_markup=kb_builder.as_markup(resize_keyboard=True))
 
 if __name__ == "__main__":
     dp.run_polling(bot)
