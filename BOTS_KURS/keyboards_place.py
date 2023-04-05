ce033fbac76fdd63d078d37f276a69b025538951
Nigan <brodvey77@gmail.com>
2023-04-05 14:51:25 +0300

    keyboard

--- BOTS_KURS/keyboards_place.py
+++ BOTS_KURS/keyboards_place.py
@@ -1,7 +1,9 @@
 from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart, Text
-from aiogram.types import (KeyboardButton, Message, ReplyKeyboardMarkup, ReplyKeyboardRemove)
+from aiogram.filters import CommandStart, Text, Command
+from aiogram.types import KeyboardButton, Message, ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButtonPollType
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.types.web_app_info import WebAppInfo
+
 
 
 
@@ -12,16 +14,32 @@ bot: Bot = Bot(token=API_TOKEN)
 dp: Dispatcher = Dispatcher()
 kb_builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
 
+# # Создаем кнопку
+# web_app_btn: KeyboardButton = KeyboardButton(text='Start Web App', web_app=WebAppInfo(url="https://you.com/"))
+#
+# # Создаем объект клавиатуры
+# web_app_keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard=[[web_app_btn]], resize_keyboard=True)
+#
+#
+# # Этот хэндлер будет срабатывать на команду "/web_app"
+# @dp.message(Command(commands='web_app'))
+# async def process_web_app_command(message: Message):
+#     await message.answer(text='Экспериментируем со специальными кнопками', reply_markup=web_app_keyboard)
 
-buttons_1: list[KeyboardButton] = [KeyboardButton(
-    text=f'button {i + 1}') for i in range(10)]
+# poll_btn: KeyboardButton = KeyboardButton(text='Create poll', request_poll=KeyboardButtonPollType(type='regular'))
+#
+# quiz_btn: KeyboardButton = KeyboardButton(text='Start quiz', request_poll=KeyboardButtonPollType(type='quiz)'))
+#
+#
+# kb_builder.row(poll_btn, quiz_btn, width=1)
+#
+# keyboard: ReplyKeyboardMarkup = kb_builder.as_markup(resize_keyboard=True)
+#
+# @dp.message(CommandStart())
+# async def process_start_command(message: Message):
+#     await message.answer(text='This is special buttons', reply_markup=keyboard)
 
-kb_builder.add(*buttons_1)
-kb_builder.adjust(2, 1, repeat=True)
 
-@dp.message(CommandStart())
-async def start(message: Message):
-    await message.answer(text='MY KEYBOARD', reply_markup=kb_builder.as_markup(resize_keyboard=True))
 
 if __name__ == "__main__":
     dp.run_polling(bot)
