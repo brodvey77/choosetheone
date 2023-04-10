c609c5f3f245941a7d103f80c5069c5ae037d181
Nigan <brodvey77@gmail.com>
2023-04-10 15:41:28 +0300

    add rock bot

--- BOTS_KURS/rock_paper_scissors_bot/handlers/user_handlers.py
+++ BOTS_KURS/rock_paper_scissors_bot/handlers/user_handlers.py
@@ -7,6 +7,7 @@ from services.services import get_bot_choice, get_winner
 
 router: Router = Router()
 
+
 # Этот хэндлер срабатывает на команду /start
 @router.message(CommandStart())
 async def process_start_command(message: Message):
@@ -14,7 +15,7 @@ async def process_start_command(message: Message):
 
 
 # Этот хэндлер срабатывает на команду /help
-@router.message(Command(commands=['/help']))
+@router.message(Command(commands=['help']))
 async def process_help_command(message: Message):
     await message.answer(text=LEXICON_RU['/help'], reply_markup=yes_no_kb)
 
@@ -25,24 +26,22 @@ async def process_yes_answer(message: Message):
     await message.answer(text=LEXICON_RU['yes'], reply_markup=game_kb)
 
 
-
 # Этот хэндлер срабатывает на отказ пользователя играть в игру
 @router.message(Text(text=LEXICON_RU['no_button']))
 async def process_no_answer(message: Message):
     await message.answer(text=LEXICON_RU['no'])
 
 
-
 # Этот хэндлер срабатывает на любую из игровых кнопок
-@router.message(Text(text=[LEXICON_RU['rock'],LEXICON_RU['paper'],LEXICON_RU['scissors']]))
+@router.message(Text(text=[LEXICON_RU['rock'],
+                           LEXICON_RU['paper'],
+                           LEXICON_RU['scissors']]))
 async def process_game_button(message: Message):
     bot_choice = get_bot_choice()
     await message.answer(text=f'{LEXICON_RU["bot_choice"]} '
-                         f'- {LEXICON_RU[bot_choice]}')
+                              f'- {LEXICON_RU[bot_choice]}')
     winner = get_winner(message.text, bot_choice)
     await message.answer(text=LEXICON_RU[winner], reply_markup=yes_no_kb)
-
-
 
 
 
--- BOTS_KURS/rock_paper_scissors_bot/services/services.py
+++ BOTS_KURS/rock_paper_scissors_bot/services/services.py
@@ -1,11 +1,11 @@
 import random
+
 from lexicon.lexicon import LEXICON_RU
 
 
 # Функция, возвращающая случайный выбор бота в игре
 def get_bot_choice() -> str:
     return random.choice(['rock', 'paper', 'scissors'])
-
 
 
 # Функция, возвращающая ключ из словаря, по которому
@@ -20,10 +20,12 @@ def _normalize_user_answer(user_answer: str) -> str:
 # Функция, определяющая победителя
 def get_winner(user_choice: str, bot_choice: str) -> str:
     user_choice = _normalize_user_answer(user_choice)
-    rules: dict[str, str] = {'rock': 'scissors', 'scissors': 'paper', 'paper': 'rock'}
+    rules: dict[str, str] = {'rock': 'scissors',
+                             'scissors': 'paper',
+                             'paper': 'rock'}
     if user_choice == bot_choice:
-        return 'nobody won'
+        return 'nobody_won'
     elif rules[user_choice] == bot_choice:
-        return 'user won'
+        return 'user_won'
     else:
-        'bot won'
+        return 'bot_won'
