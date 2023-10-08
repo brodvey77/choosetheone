b21ac48f23da3cd6dad076a5e4fbc123ea6b3690
Nigan_L <s264157250r@gmail.com>
2023-10-08 14:44:23 +0300

    json_445

--- Stepik_3_python/json_all.py
+++ Stepik_3_python/json_all.py
@@ -75,18 +75,55 @@
 
 
 
+# import json
+
+# club1 = {"name": "FC Byern Munchen", "country": "Germany", "founded": 1900,
+#          "trainer": "Julian Nagelsmann", "goalkeeper": "M. Neuer", "league_position": 1}
+
+# club2 = {"name": "FC Barcelona", "country": "Spain", "founded": 1899,
+#          "trainer": "Xavier Creus", "goalkeeper": "M. Ter Stegen", "league_position": 7}
+
+# club3 = {"name": "FC Manchester United", "country": "England", "founded": 1878,
+#          "trainer": "Michael Carrick", "goalkeeper": "D. De Gea", "league_position": 8}
+
+# my_list = [club1, club2, club3]
+
+# with open('data.json', 'w') as file:
+#     json.dump(my_list, file, indent='   ')
+
+
+# import json
+
+# specs = {
+#          'Модель': 'AMD Ryzen 5 5600G',
+#          'Год релиза': 2021,
+#          'Сокет': 'AM4',
+#          'Техпроцесс': '7 нм',
+#          'Ядро': 'Cezanne',
+#          'Объем кэша L2': '3 МБ',
+#          'Объем кэша L3': '16 МБ',
+#          'Базовая частота': '3900 МГц'
+#         }
+
+# specs_json = json.dumps(specs, ensure_ascii=False, indent='   ')
+
+# print(specs_json)
+
+
+
 import json
 
-club1 = {"name": "FC Byern Munchen", "country": "Germany", "founded": 1900,
-         "trainer": "Julian Nagelsmann", "goalkeeper": "M. Neuer", "league_position": 1}
+def is_correct_json(string):
+        try:
+                json.dumps(string)
+                return True
+        except:
+                return False
 
-club2 = {"name": "FC Barcelona", "country": "Spain", "founded": 1899,
-         "trainer": "Xavier Creus", "goalkeeper": "M. Ter Stegen", "league_position": 7}
 
-club3 = {"name": "FC Manchester United", "country": "England", "founded": 1878,
-         "trainer": "Michael Carrick", "goalkeeper": "D. De Gea", "league_position": 8}
 
-my_list = [club1, club2, club3]
+a = input()
 
-with open('data.json', 'w') as file:
-    json.dump(my_list, file, indent='   ')
+
+
+print(is_correct_json(a))
