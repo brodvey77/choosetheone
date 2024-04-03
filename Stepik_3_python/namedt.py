4536ff397e3aac777c87dd044765496ba4bb1f4a
Nigan <brodvey77@gmail.com>
2024-04-03 11:52:03 +0300

    6_4_9

---
+++ Stepik_3_python/namedt.py
@@ -0,0 +1,18 @@
+# from collections import namedtuple
+#
+# Fruit = namedtuple('Fruit', ['name', 'color', 'vitamins'])
+#
+# print(Fruit)
+
+
+# from collections import namedtuple
+#
+# Game = namedtuple('Game', 'name developer publisher')
+#
+# ExtendedGame = namedtuple('ExtendedGame', [*Game._fields, 'release_date', 'price'])
+#
+# game = Game('GENERALS', 'Серёга', 'HDD')
+# print(game)
+# game2 = ExtendedGame('GENERALS', 'Серёга', 'HDD', '250565', 250000)
+# print(game2)
+
