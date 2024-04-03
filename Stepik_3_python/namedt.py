6e011f599f7a13cf071acbb8c9479073dbbecb65
Nigan <brodvey77@gmail.com>
2024-04-03 12:57:50 +0300

    add datapikle

--- Stepik_3_python/namedt.py
+++ Stepik_3_python/namedt.py
@@ -16,3 +16,13 @@
 # game2 = ExtendedGame('GENERALS', 'Серёга', 'HDD', '250565', 250000)
 # print(game2)
 
+from collections import namedtuple
+import pickle
+
+Animal = namedtuple('Animal', ['name', 'family', 'sex', 'color'])
+
+with open('data.pkl', 'rb') as file:
+    data = pickle.load(file)
+
+    for i in data:
+        print(i)
