6ca84fa9ceaa3c083b94937e2ff6bea2b346bf99
Nigan <brodvey77@gmail.com>
2024-04-08 15:29:02 +0300

    defoultdict

--- Stepik_3_python/namedt.py
+++ Stepik_3_python/namedt.py
@@ -105,6 +105,13 @@
 
 from collections import defaultdict
 
-
-print(type(my_defdict))
-
+# from collections import defaultdict
+#
+# data = defaultdict(list)
+# print(data['key1'])
+#
+# data.default_factory = dict
+# print(data['key2'])
+#
+# data.default_factory = tuple
+# print(data['key3'])
