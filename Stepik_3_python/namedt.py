0b8a3a9cd73080041b0fa9577c3a1bd227809a99
Nigan <brodvey77@gmail.com>
2024-04-09 10:41:57 +0300

    defoultdict 6_5_19

--- Stepik_3_python/namedt.py
+++ Stepik_3_python/namedt.py
@@ -103,7 +103,7 @@
 #     for i in sorted(data, key=lambda x: dt.strptime(f'{x["meeting_date"]} {x["meeting_time"]}', s)):
 #         print(i['surname'], i['name'])
 
-from collections import defaultdict
+# from collections import defaultdict
 
 # from collections import defaultdict
 #
@@ -115,3 +115,28 @@ from collections import defaultdict
 #
 # data.default_factory = tuple
 # print(data['key3'])
+
+#
+from collections import defaultdict
+
+# data = [('Books', 1343), ('Books', 1166), ('Merch', 616), ('Courses', 966), ('Merch', 1145), ('Courses', 1061), ('Books', 848), ('Courses', 964), ('Tutorials', 832), ('Merch', 642), ('Books', 815), ('Tutorials', 1041), ('Books', 1218), ('Tutorials', 880), ('Books', 1003), ('Merch', 951), ('Books', 920), ('Merch', 729), ('Tutorials', 977), ('Books', 656)]
+#
+# d = {}
+#
+# for i in data:
+#     d[i[0]] = d.get(i[0], 0) + i[1]
+#
+# for k,v in sorted(d.items()):
+#     print(f'{k}: ${v}')
+
+# from collections import defaultdict
+#
+# data = [('Books', 1343), ('Books', 1166), ('Merch', 616), ('Courses', 966), ('Merch', 1145), ('Courses', 1061), ('Books', 848), ('Courses', 964), ('Tutorials', 832), ('Merch', 642), ('Books', 815), ('Tutorials', 1041), ('Books', 1218), ('Tutorials', 880), ('Books', 1003), ('Merch', 951), ('Books', 920), ('Merch', 729), ('Tutorials', 977), ('Books', 656)]
+#
+# result = defaultdict(int)
+#
+# for key, value in data:
+#     result[key] += value
+#
+# for key, value in sorted(result.items()):
+#     print(f'{key}: ${value}')
