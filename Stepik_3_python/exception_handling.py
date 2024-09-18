0a1451dc03985ebed66988364df7bf2517b9ea5c
Nigan <brodvey77@gmail.com>
2024-09-18 16:35:27 +0300

    add excent_2

--- Stepik_3_python/exception_handling.py
+++ Stepik_3_python/exception_handling.py
@@ -1,40 +1,19 @@
-# text = 'Hello, world!'
+# try:
+#     num1 = int(input())
+#     num2 = int(input())
+#     print('Частное чисел равно', num1 / num2)
+# except:
+#     print('Вы ввели некорректные данные!')
 #
-# print(text.find('w'))
-# print(text.find('a'))
-# text = 'Hello, world!'
+# print('Работа программы завершена!10')
 #
-# print(text.index('a'))
-
-
-# total = 0
+# try:
+#     num1 = int(input())
+#     num2 = int(input())
+#     print('Частное чисел равно', num1 / num2)
+# except ValueError:
+#     print('Нужно было ввести числа!')
+# except ZeroDivisionError:
+#     print('На ноль делить нельзя!')
 #
-# with open('test_1.txt', encoding='utf-8') as file:
-#     for _ in file.readlines():
-#         total = total + 1
-#
-# print(total)
-
-# def swapcase_vowels(string):
-#     vowels = 'aeiouy'
-#     swapped_string = ''
-#
-#     for char in string:
-#         if char in vowels:
-#             swapped_string += char.upper()
-#         else:
-#             swapped_string += char
-#
-#     return swapped_string
-#
-# print(swapcase_vowels('This bank says ‘millions’ of people could be targeted by AI voice-cloning scams'))
-
-# a = int(input())
-# b = int(input())
-# numbers = []
-#
-# for i in range(a, b + 1):
-#     if i % 7 == 0:
-#         numbers.append(i)
-#
-# print(numbers)
+# print('Работа программы завершена!')
