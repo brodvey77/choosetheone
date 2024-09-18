488b1de66ec20f7e502496508c398cb869e02ee6
Nigan <brodvey77@gmail.com>
2024-09-18 14:29:45 +0300

    add exception handling file

---
+++ Stepik_3_python/exception_handling.py
@@ -0,0 +1,16 @@
+# text = 'Hello, world!'
+#
+# print(text.find('w'))
+# print(text.find('a'))
+# text = 'Hello, world!'
+#
+# print(text.index('a'))
+
+
+total = 0
+
+with open(test_1.txt, encoding='utf-8') as file:
+    for _ in file.readlines:
+        total == total + 1
+
+print(total)
