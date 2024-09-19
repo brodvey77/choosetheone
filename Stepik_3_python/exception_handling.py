5a6a8647438a46a1dcce75d63e63522add37d569
Nigan <brodvey77@gmail.com>
2024-09-19 12:43:26 +0300

    add 7_2_22

--- Stepik_3_python/exception_handling.py
+++ Stepik_3_python/exception_handling.py
@@ -75,3 +75,17 @@
 #         fifth.append('_')
 #
 # print(fifth)
+
+
+# numbers = [6, 0, 36, 8, 2, 36, 0, 12, 60, 0, 45, 0, 3, 23]
+#
+# remainders = []
+#
+# for number in numbers:
+#     try:
+#         remainders.append(36 % number)
+#     except ZeroDivisionError:
+#         pass
+# print(remainders)
+
+
