cb54e1cf33b2bed65e9fda57d54f54f3e9412031
Nigan <brodvey77@gmail.com>
2024-09-19 12:03:06 +0300

    add 7_2_21

--- Stepik_3_python/exception_handling.py
+++ Stepik_3_python/exception_handling.py
@@ -65,4 +65,13 @@
 #
 # print(total_likes)
 
-
+# food = ['chocolate', 'chicken', 'corn', 'sandwich', 'soup', 'potatoes', 'beef', 'lox', 'lemonade']
+# fifth = []
+#
+# for x in food:
+#     try:
+#         fifth.append(x[4])
+#     except IndexError:
+#         fifth.append('_')
+#
+# print(fifth)
