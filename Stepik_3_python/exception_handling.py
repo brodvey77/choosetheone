43c009f9db0f21555fa6f0c3f769c7d18b190745
Nigan <brodvey77@gmail.com>
2024-09-26 11:27:57 +0300

    add 7_4_18

--- Stepik_3_python/exception_handling.py
+++ Stepik_3_python/exception_handling.py
@@ -344,3 +344,21 @@ import calendar
 # except KeyError as e:
 #     print(e.args)
 
+
+# week = { 1: "Понедельник", 2: "Вторник", 3: "Среда", 4: "Четверг", 5: "Пятница", 6: "Суббота", 7: "Воскресенье", }
+# def get_weekday(number):
+#     if not isinstance(number, int):
+#         raise TypeError('Аргумент не является целым числом')
+#     if number < 1 or number > 7:
+#         raise ValueError('Аргумент не принадлежит требуемому диапазону')
+#     else:
+#         return week[int(number)]
+#
+#
+# try:
+#     print(get_weekday(8))
+# except ValueError as err:
+#     print(err)
+#     print(type(err))
+
+
