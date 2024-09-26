192ad7534c73985554c916197bc252ae27f1a0d5
Nigan <brodvey77@gmail.com>
2024-09-26 10:47:56 +0300

    add 7_4_17

--- Stepik_3_python/exception_handling.py
+++ Stepik_3_python/exception_handling.py
@@ -332,3 +332,15 @@ import calendar
 # except ValueError as e:
 #     print(e)
 #     print(e.args)
+
+# data = {'Chrome': 'browser', 'Discord': 'messenger', 'Steam': 'digital distributor'}
+#
+# try:
+#     try:
+#         app_name = 'Firefox'
+#         app_type = data[app_name]
+#     except:
+#         raise KeyError('Not found', app_name)
+# except KeyError as e:
+#     print(e.args)
+
