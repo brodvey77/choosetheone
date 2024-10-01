3a0b851e495c545a7e24a22150e47c8ceb9e973f
Nigan <brodvey77@gmail.com>
2024-10-01 12:56:27 +0300

    recursion

---
+++ Stepik_3_python/recursion.py
@@ -0,0 +1,86 @@
+# def message():
+#     print('Это рекурсивная функция')
+#     message()
+#
+# message()
+
+# def bee(n):
+#     if n > 0:
+#         print(n)
+#         bee(n - 1)
+#
+# bee(3)
+
+# def bee(n):
+#     if n > 0:
+#         bee(n - 1)
+#         print(n)
+#
+# bee(3)
+
+# def bee(n):
+#     if n > 0:
+#         print(n)
+#         bee(n - 1)
+#         print(n)
+#
+# bee(2)
+
+
+# def bee(n):
+#     if n > 0:
+#         print(n)
+#         bee(n - 1)
+#     print(n)
+#
+# bee(2)
+
+
+# def bee(n):
+#     print(n)
+#     if n > 0:
+#         print(n)
+#         bee(n - 1)
+#
+# bee(2)
+
+# def beegeek(n):
+#     if n > 0:
+#         print('bee')
+#         beegeek(n - 1)
+#     else:
+#         print('geek')
+#
+# beegeek(3)
+
+
+# def bee(n):
+#     if n >= 777:
+#         print('bee')
+#     else:
+#         bee(n + 1)
+#
+# bee(666)
+
+
+# def bee(n):
+#     if n >= 7:
+#         print('bee')
+#     else:
+#         print(n)
+#         bee(n + 1)
+#
+#
+# bee(4)
+
+# def bee(n):
+#     if n >= 7:
+#         print('bee')
+#     else:
+#         bee(n + 1)
+#         print(n)
+#
+#
+# bee(4)
+
+
