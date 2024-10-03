785841c45d580f7ecb92925fe332fb1649df51e3
redos <s264157250r@gmail.com>
2024-10-03 21:30:04 +0300

    8_3_11

--- Stepik_3_python/recursion.py
+++ Stepik_3_python/recursion.py
@@ -346,3 +346,15 @@
 #     assert get_pow(2, 10) == 1024
 #
 #     assert get_pow(99, 0) == 1
+
+def get_fast_pow(a, n):
+    if n == 0:
+        return 1
+    elif n % 2 == 1: 
+        return a * get_fast_pow(a, n - 1)
+    else:
+        return get_fast_pow(a *a,  n // 2)
+
+
+
+print(get_fast_pow(2, 10))
