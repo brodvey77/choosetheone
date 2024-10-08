f806f8a46b8bf64c35bbbe667cb23d85211dad74
Nigan <brodvey77@gmail.com>
2024-10-08 12:24:53 +0300

    8_3_14

--- Stepik_3_python/recursion.py
+++ Stepik_3_python/recursion.py
@@ -389,6 +389,54 @@
 #     assert is_power(128) == True
 #     assert is_power(1024) == True
 #     assert is_power(1111111) == False
+#
+# def tribonacci(n):
+#     cache = {1: 1, 2: 1, 3: 1}
+#     def fib_rec(n):
+#         result = cache.get(n)
+#         if result is None:
+#             result = fib_rec(n - 3) + fib_rec( n - 2) + fib_rec(n - 1)
+#             cache[n] = result
+#         return result
+#     return fib_rec(n)
+#
+#
+#
+#
+#
+#
+# # TEST_1:
+# print(tribonacci(1))
+#
+# # TEST_2:
+# print(tribonacci(7))
+#
+# # TEST_3:
+# print(tribonacci(4))
+#
+# # TEST_4:
+# print(tribonacci(3))
+#
+# # TEST_5:
+# print(tribonacci(10))
+#
+# # TEST_6:
+# print(tribonacci(2))
+#
+# # TEST_7:
+# print(tribonacci(300))
+#
+# # TEST_8:
+# print(tribonacci(100))
+
+
+
+
+
+
+
+
+
 
 
 
