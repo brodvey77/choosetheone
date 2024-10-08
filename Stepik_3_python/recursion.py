a9a33cbff3a9f617550573e24eb5f041658b6540
Nigan <brodvey77@gmail.com>
2024-10-08 13:47:50 +0300

    8_3_16

--- Stepik_3_python/recursion.py
+++ Stepik_3_python/recursion.py
@@ -469,11 +469,42 @@
 # print(is_palindrome('abcca'))
 
 
-
-
-
-
-
+# def to_binary(number):
+#     if number == 0:
+#         return "0"
+#     if number == 1:
+#         return "1"
+#     else:
+#         return to_binary(number // 2) + str(number % 2)
+#
+#
+#
+# # TEST_1:
+# print(to_binary(15))
+#
+# # TEST_2:
+# print(to_binary(0))
+#
+# # TEST_3:
+# print(to_binary(1))
+#
+# # TEST_4:
+# print(to_binary(256))
+#
+# # TEST_5:
+# print(to_binary(2))
+#
+# # TEST_6:
+# print(to_binary(1025))
+#
+# # TEST_7:
+# print(to_binary(3427))
+#
+# # TEST_8:
+# print(to_binary(131445))
+#
+# # TEST_9:
+# print(to_binary(532))
 
 
 
