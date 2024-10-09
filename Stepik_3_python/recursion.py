ce160f54135e2e3881e065e1eb77e26df7913a9c
Nigan <brodvey77@gmail.com>
2024-10-09 14:35:14 +0300

    8_4_6

--- Stepik_3_python/recursion.py
+++ Stepik_3_python/recursion.py
@@ -677,3 +677,28 @@
 #
 # my_list = [3, [4], [5, [6, [7, 8]]]]
 # print(linear(my_list))
+
+
+def get_value(nested_dicts, key):
+    if key in nested_dicts:
+        return nested_dicts.get(key)
+    for v in nested_dicts.values():
+        if isinstance(v, dict):
+            value = get_value(v, key)
+            if value is not None:
+                return value
+
+
+data = {'firstName': 'Тимур', 'lastName': 'Гуев', 'birthDate': {'day': 10, 'month': 'October', 'year': 1993}, 'address': {'streetAddress': 'Часовая 25, кв. 127', 'city': {'region': 'Московская область', 'type': 'город', 'cityName': 'Москва'}, 'postalCode': '125315'}}
+print(get_value(data, 'cityName'))
+
+
+
+#     for v in data.values():
+#         if type(v) == dict:
+#             value = find_key(v, key)  # рекурсивный случай
+#             if value is not None:
+#                 return value
+
+data = {'first_name': 'Alyson', 'last_name': 'Hannigan', 'birthday': {'day': 24, 'month': 'March', 'year': 1974}}
+print(get_value(data, 'birthday'))
