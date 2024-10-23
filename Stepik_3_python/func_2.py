355d0d5ab0b642a9df632e990105b7cc4895278d
Nigan <brodvey77@gmail.com>
2024-10-23 12:27:45 +0300

    9_5_19

--- Stepik_3_python/func_2.py
+++ Stepik_3_python/func_2.py
@@ -933,3 +933,25 @@ import struct
 #
 # # TEST_5:
 # https://hide_and_seek.harvard?magic_wand=elderberry&thief=Volandemord&wizard=Dambldor
+
+
+
+# from datetime import date
+# loc = {
+#     'ru': '%d.%m.%Y',
+#     'us': '%m-%d-%Y',
+#     'ca': '%Y-%m-%d',
+#     'br': '%d/%m/%Y',
+#     'fr': '%d.%m.%Y',
+#     'pt': '%d-%m-%Y'
+# }
+# def date_formatter(country_code):
+#     def lang_ru(d):
+#         return date.strftime(d, loc[country_code])
+#     return lang_ru
+#
+#
+#
+# date_ru = date_formatter('ru')
+# today = date(2022, 1, 25)
+# print(date_ru(today))
