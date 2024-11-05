b6ef7e15f42b7ee5be9f4687f82f13c3b847bed2
Nigan <brodvey77@gmail.com>
2024-11-05 16:46:39 +0300

    9_8_25

--- Stepik_3_python/decorators.py
+++ Stepik_3_python/decorators.py
@@ -1952,130 +1952,344 @@ import functools
 #
 # print(func.func_attr)
 
+#
+# import functools
+# def ignore_exception(*args):
+#     def decorator(func):
+#         @functools.wraps(func)
+#         def wrapper(*aargs, **kkwargs):
+#             try:
+#                 return func(*aargs, **kkwargs)
+#             except (args) as e:
+#                 print(f'Исключение {e.__class__.__name__} обработано')
+#
+#
+#         return wrapper
+#     return decorator
+#
+#
+# # INPUT DATA:
+#
+# # TEST_1:
+# @ignore_exception(ZeroDivisionError, TypeError, ValueError)
+# def f(x):
+#     return 1 / x
+#
+#
+# f(0)
+#
+# # TEST_2:
+# min = ignore_exception(ZeroDivisionError)(min)
+#
+# try:
+#     print(min(1, '2', 3, [4, 5]))
+# except Exception as e:
+#     print(type(e))
+#
+#
+# # TEST_3:
+# @ignore_exception()
+# def func():
+#     '''func docs'''
+#     raise ValueError
+#
+#
+# try:
+#     func()
+# except Exception as e:
+#     print(type(e))
+#
+#
+# # TEST_4:
+# @ignore_exception(TypeError)
+# def func():
+#     '''func docs'''
+#     raise ValueError
+#
+#
+# try:
+#     func()
+# except Exception as e:
+#     print(type(e))
+#
+#
+# # TEST_5:
+# @ignore_exception(ValueError, TypeError, NameError)
+# def func():
+#     '''func docs'''
+#     raise ValueError
+#
+#
+# print(func.__name__)
+# print(func.__doc__)
+#
+# try:
+#     func()
+# except Exception as e:
+#     print(type(e))
+#
+#
+# # TEST_6:
+# @ignore_exception(ValueError, TypeError, NameError)
+# def func():
+#     '''func docs'''
+#     raise NameError
+#
+#
+# try:
+#     func()
+# except Exception as e:
+#     print(type(e))
+#
+#
+# # TEST_7:
+# @ignore_exception(ValueError, TypeError, ZeroDivisionError, NameError)
+# def func():
+#     '''func docs'''
+#     raise ZeroDivisionError
+#
+#
+# try:
+#     func()
+# except Exception as e:
+#     print(type(e))
+#
+#
+# # TEST_8:
+# @ignore_exception(ValueError, NameError, ZeroDivisionError, TypeError)
+# def func(a, b, c):
+#     '''func docs'''
+#     raise NameError
+#
+#
+# try:
+#     func(1, 2, c=10)
+# except Exception as e:
+#     print(type(e))
+#
+#
+# # TEST_9:
+# @ignore_exception(ValueError, TypeError, ZeroDivisionError, NameError)
+# def beegeek():
+#     return 'beegeek'
+#
+#
+# print(beegeek())
 
-import functools
-def ignore_exception(*args):
-    def decorator(func):
-        @functools.wraps(func)
-        def wrapper(*aargs, **kkwargs):
-            try:
-                return func(*aargs, **kkwargs)
-            except (args) as e:
-                print(f'Исключение {e.__class__.__name__} обработано')
 
-
-        return wrapper
-    return decorator
-
-
-# INPUT DATA:
-
-# TEST_1:
-@ignore_exception(ZeroDivisionError, TypeError, ValueError)
-def f(x):
-    return 1 / x
-
-
-f(0)
-
-# TEST_2:
-min = ignore_exception(ZeroDivisionError)(min)
-
-try:
-    print(min(1, '2', 3, [4, 5]))
-except Exception as e:
-    print(type(e))
-
-
-# TEST_3:
-@ignore_exception()
-def func():
-    '''func docs'''
-    raise ValueError
-
-
-try:
-    func()
-except Exception as e:
-    print(type(e))
-
-
-# TEST_4:
-@ignore_exception(TypeError)
-def func():
-    '''func docs'''
-    raise ValueError
-
-
-try:
-    func()
-except Exception as e:
-    print(type(e))
-
-
-# TEST_5:
-@ignore_exception(ValueError, TypeError, NameError)
-def func():
-    '''func docs'''
-    raise ValueError
-
-
-print(func.__name__)
-print(func.__doc__)
-
-try:
-    func()
-except Exception as e:
-    print(type(e))
-
-
-# TEST_6:
-@ignore_exception(ValueError, TypeError, NameError)
-def func():
-    '''func docs'''
-    raise NameError
-
-
-try:
-    func()
-except Exception as e:
-    print(type(e))
-
-
-# TEST_7:
-@ignore_exception(ValueError, TypeError, ZeroDivisionError, NameError)
-def func():
-    '''func docs'''
-    raise ZeroDivisionError
-
-
-try:
-    func()
-except Exception as e:
-    print(type(e))
-
-
-# TEST_8:
-@ignore_exception(ValueError, NameError, ZeroDivisionError, TypeError)
-def func(a, b, c):
-    '''func docs'''
-    raise NameError
-
-
-try:
-    func(1, 2, c=10)
-except Exception as e:
-    print(type(e))
-
-
-# TEST_9:
-@ignore_exception(ValueError, TypeError, ZeroDivisionError, NameError)
-def beegeek():
-    return 'beegeek'
-
-
-print(beegeek())
-
-
+# import functools
+#
+# class MaxRetriesException(Exception):
+#     pass
+#
+# def retry(times):
+#     def decorator(func):
+#         @functools.wraps(func)
+#         def wrapper(*args, **kwargs):
+#             for i in range(1, times +1):
+#                 try:
+#                     return func(*args, **kwargs)
+#                 except: pass
+#             raise MaxRetriesException
+#
+#
+#         return wrapper
+#     return decorator
+#
+#
+#
+# # INPUT DATA:
+#
+# # TEST_1:
+# @retry(3)
+# def no_way():
+#     raise ValueError
+#
+# try:
+#     no_way()
+# except Exception as e:
+#     print(type(e))
+#
+# # TEST_2:
+# @retry(8)
+# def beegeek():
+#     beegeek.calls = beegeek.__dict__.get('calls', 0) + 1
+#     if beegeek.calls < 5:
+#         raise ValueError
+#     print('beegeek')
+#
+# beegeek()
+#
+# # TEST_3:
+# @retry(6)
+# def beegeek():
+#     beegeek.calls = beegeek.__dict__.get('calls', 0) + 1
+#     if beegeek.calls < 7:
+#         raise ValueError
+#     print('beegeek')
+#
+# try:
+#     beegeek()
+# except Exception as e:
+#     print(type(e))
+#
+# # TEST_4:
+# @retry(7)
+# def beegeek():
+#     beegeek.calls = beegeek.__dict__.get('calls', 0) + 1
+#     if beegeek.calls < 7:
+#         raise ValueError
+#     print('beegeek')
+#
+# try:
+#     beegeek()
+# except Exception as e:
+#     print(type(e))
+#
+# # TEST_5:
+# @retry(5)
+# def beegeek():
+#     beegeek.calls = beegeek.__dict__.get('calls', 0) + 1
+#     if beegeek.calls < 7:
+#         raise ValueError
+#     print('beegeek')
+#
+# try:
+#     beegeek()
+# except Exception as e:
+#     print(type(e))
+#
+# # TEST_6:
+# @retry(5)
+# def beegeek():
+#     beegeek.calls = beegeek.__dict__.get('calls', 0) + 1
+#     if beegeek.calls < 7:
+#         raise ValueError
+#     print('beegeek')
+#
+# try:
+#     beegeek()
+# except Exception as e:
+#     print(type(e))
+#
+# # TEST_7:
+# @retry(9)
+# def add(a, b):
+#     add.calls = add.__dict__.get('calls', 0) + 1
+#     if add.calls < 10:
+#         raise ValueError
+#     return a + b
+#
+# try:
+#     print(add(10, 20))
+# except Exception as e:
+#     print(type(e))
+#
+# # TEST_8:
+# @retry(10)
+# def add(a, b):
+#     add.calls = add.__dict__.get('calls', 0) + 1
+#     if add.calls < 10:
+#         raise ValueError
+#     return a + b
+#
+# try:
+#     print(add(10, 30))
+# except Exception as e:
+#     print(type(e))
+#
+# # TEST_9:
+# @retry(100)
+# def add(a, b):
+#     add.calls = add.__dict__.get('calls', 0) + 1
+#     if add.calls < 10:
+#         raise ValueError
+#     return a + b
+#
+# try:
+#     print(add(40, 10))
+# except Exception as e:
+#     print(type(e))
+#
+# # TEST_10:
+# @retry(1)
+# def add(a, b):
+#     add.calls = add.__dict__.get('calls', 0) + 1
+#     if add.calls < 10:
+#         raise ValueError
+#     return a + b
+#
+# try:
+#     print(add(40, 20))
+# except Exception as e:
+#     print(type(e))
+#
+# # TEST_11:
+# @retry(10)
+# def calculate(a, b, c):
+#     calculate.calls = calculate.__dict__.get('calls', 0) + 1
+#     if calculate.calls < 4:
+#         raise ValueError
+#     return a + b + c
+#
+# try:
+#     print(calculate(1, 2, c=3))
+# except Exception as e:
+#     print(type(e))
+#
+# # TEST_12:
+# @retry(3)
+# def calculate(a, b, c):
+#     calculate.calls = calculate.__dict__.get('calls', 0) + 1
+#     if calculate.calls < 4:
+#         raise ValueError
+#     return a + b + c
+#
+# try:
+#     print(calculate(b=1, a=2, c=3))
+# except Exception as e:
+#     print(type(e))
+#
+# # TEST_13:
+# @retry(4)
+# def calculate(a, b, c):
+#     calculate.calls = calculate.__dict__.get('calls', 0) + 1
+#     if calculate.calls < 4:
+#         raise ValueError
+#     return a + b + c
+#
+# try:
+#     print(calculate(b=2, a=2, c=3))
+# except Exception as e:
+#     print(type(e))
+#
+# # TEST_14:
+# @retry(99)
+# def calculate(a, b, c):
+#     calculate.calls = calculate.__dict__.get('calls', 0) + 1
+#     if calculate.calls < 4:
+#         raise ValueError
+#     return a + b + c
+#
+# try:
+#     print(calculate(10, b=1, c=2))
+# except Exception as e:
+#     print(type(e))
+#
+# # TEST_15:
+# @retry(99)
+# def calculate(a, b, c):
+#     """Calculate something"""
+#     calculate.calls = calculate.__dict__.get('calls', 0) + 1
+#     if calculate.calls < 4:
+#         raise ValueError
+#     return a + b + c
+#
+# print(calculate.__name__)
+# print(calculate.__doc__)
+# try:
+#     print(calculate(2000, b=1, c=4))
+# except Exception as e:
+#     print(type(e))
 
 
