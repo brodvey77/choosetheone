ee69dd2449a6c25f67a9b5852f36011b5826d4ae
Nigan <brodvey77@gmail.com>
2024-11-11 15:29:26 +0300

    10_1_20

--- Stepik_3_python/iterators.py
+++ Stepik_3_python/iterators.py
@@ -74,3 +74,20 @@
 #
 #
 # print(next(a))
+
+# numbers = [100, 70, 34, 45, 30, 83, 12, 83, -28, 49, -8, -2, 6, 62, 64, -22, -19, 61, 13, 5, 80, -17, 7, 3, 21, 73, 88, -11, 16, -22]
+#
+# a = iter(numbers[::-1])
+# print(next(a))
+# print(next(a))
+# print(next(iter(reversed(numbers))))
+#
+#
+# numbers = [100, 70, 34, 45, 30, 83, 12, 83, -28, 49, -8, -2, 6, 62, 64, -22, -19, 61, 13, 5, 80, -17, 7, 3, 21, 73, 88, -11, 16, -22]
+# numbers = iter(numbers)
+# while True:
+#     try:
+#         n = next(numbers)
+#     except StopIteration:
+#         print(n)
+#         break
