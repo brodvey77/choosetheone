0cf7d2c607eaf90b64f6f13728dff75325ae4be3
Nigan <brodvey77@gmail.com>
2024-11-18 12:03:32 +0300

    10_4_12

--- Stepik_3_python/iterators.py
+++ Stepik_3_python/iterators.py
@@ -1033,3 +1033,74 @@ import copy
 #
 # print(next(power_of_two))
 
+class DictItemsIterator:
+    def __init__(self, data):
+        self._data = data
+        self._keys = list(data)
+        self._index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._index < len(self._keys):
+            key = self._keys[self._index]
+            value = self._data[key]
+            self._index += 1
+            return (key, value)
+        else:
+            raise StopIteration
+
+
+
+
+# INPUT DATA:
+
+# TEST_1:
+pairs = DictItemsIterator({1: 'A', 2: 'B', 3: 'C'})
+
+print(*pairs)
+
+# TEST_2:
+data = {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49}
+
+pairs = DictItemsIterator(data)
+
+print(*pairs)
+
+# TEST_3:
+data = {'Arthur': 100, 'Timur': 100, 'Dima': 100,
+        'Anri': 101, 'Roma': 99, 'German': 98}
+
+pairs = DictItemsIterator(data)
+
+print(list(pairs))
+
+# TEST_4:
+data = {'Arthur': [100, 5], 'Timur': [100, 6], 'Dima': [100, 7, 8],
+        'Anri': [100, 101], 'Roma': [99]}
+
+pairs = DictItemsIterator(data)
+
+print(next(pairs))
+print(next(pairs))
+print(next(pairs))
+print(next(pairs))
+print(next(pairs))
+
+try:
+    print(next(pairs))
+except StopIteration:
+    print('Error')
+
+# TEST_5:
+data = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7}
+
+pairs = DictItemsIterator(data)
+
+print(tuple(pairs))
+
+try:
+    print(next(pairs))
+except StopIteration:
+    print('Error')
