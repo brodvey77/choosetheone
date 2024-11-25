44602a6f6445bc7fa417663432ced8ce088cd195
Nigan <brodvey77@gmail.com>
2024-11-25 13:26:01 +0300

    10_7_6

--- Stepik_3_python/generators.py
+++ Stepik_3_python/generators.py
@@ -960,5 +960,57 @@ from datetime import date, timedelta
 # print(result.name)
 
 
+# def parse_ranges(ranges):
+#     for range_str in ranges.split(','):
+#         a, b = map(int, range_str.split('-'))
+#         for number in range(a, b + 1):
+#             yield number
+#
+#
+#
+# def parse_ranges(ranges):
+#     return (j
+#             for i in ranges.split(',')
+#             for a, b in [i.split('-')]
+#             for j in range(int(a), int(b) + 1))
+#
+#
+#
+# # INPUT DATA:
+#
+# # TEST_1:
+# print(*parse_ranges('1-2,4-4,8-10'))
+#
+# # TEST_2:
+# print(*parse_ranges('1-10,2-10'))
+#
+# # TEST_3:
+# print(*parse_ranges('7-32'))
+#
+# # TEST_4:
+# print(*parse_ranges('8-8,8-8,8-8,8-8,8-8'))
+#
+# # TEST_5:
+# print(*parse_ranges('10-10,11-11,12-12'))
+#
+# # TEST_6:
+# print(*parse_ranges('10-15,1-4,4-5,5-5,1-10,1-15'))
+#
+# # TEST_7:
+# numbers = parse_ranges('1-3')
+#
+# print(next(numbers))
+# print(next(numbers))
+# print(next(numbers))
+#
+# # TEST_8:
+# numbers = parse_ranges('1-3,4-5,10-32,32-32,32-40,1-2,2-32,10-11,67-199')
+#
+# print(*numbers)
+
+
+
+
+
 
 
