78f1f212d7c3a88b7d51188c2d1ef39c4ef7f549
Nigan <brodvey77@gmail.com>
2024-11-25 12:15:03 +0300

    10_7_5

--- Stepik_3_python/generators.py
+++ Stepik_3_python/generators.py
@@ -932,3 +932,33 @@ from datetime import date, timedelta
 #
 # print(*interleave(numbers3, string2, numbers4, string1, numbers2, numbers1))
 
+
+
+# from collections import namedtuple
+#
+# Person = namedtuple('Person', ['name', 'nationality', 'sex', 'birth', 'death'])
+#
+# persons = [Person('E. M. Ashe', 'American', 'male', 1867, 1941),
+#            Person('Goran Aslin', 'Swedish', 'male', 1980, 0),
+#            Person('Erik Gunnar Asplund', 'Swedish', 'male', 1885, 1940),
+#            Person('Genevieve Asse', 'French', 'female', 1949, 0),
+#            Person('Irene Adler', 'Swedish', 'female', 2005, 0),
+#            Person('Sergio Asti', 'Italian', 'male', 1926, 0),
+#            Person('Olof Backman', 'Swedish', 'male', 1999, 0),
+#            Person('Alyson Hannigan', 'Swedish', 'female', 1940, 1987),
+#            Person('Dana Atchley', 'American', 'female', 1941, 2000),
+#            Person('Monika Andersson', 'Swedish', 'female', 1957, 0),
+#            Person('Shura_Stone', 'Russian', 'male', 2000, 0),
+#            Person('Jon Bale', 'Swedish', 'male', 2000, 0)]
+#
+#
+# alive = (i for i in persons if i.death == 0)
+# man = (i for i in alive if i.sex == 'male')
+# national = (i for i in man if i.nationality == 'Swedish')
+# result = max((i for i in national), key=lambda x: x.birth)
+#
+# print(result.name)
+
+
+
+
