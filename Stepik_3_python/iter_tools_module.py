9e90ffb1fb557a3a7614028c8952165b3ba140da
Nigan <brodvey77@gmail.com>
2024-12-11 13:02:13 +0300

    10_10_14

--- Stepik_3_python/iter_tools_module.py
+++ Stepik_3_python/iter_tools_module.py
@@ -559,3 +559,40 @@ from itertools import filterfalse, takewhile, dropwhile
 # numbers = [100, 20, 30, 400, 500, 5]*100000
 #
 # print(sum_of_digits(numbers))
+
+# from itertools import pairwise
+#
+# def is_rising(iterable):
+#     return all(a < b for a, b in pairwise(iterable))
+#
+#
+#
+# # INPUT DATA:
+#
+# # TEST_1:
+# print(is_rising([1, 2, 3, 4, 5]))
+#
+# # TEST_2:
+# iterator = iter([1, 1, 1, 2, 3, 4])
+#
+# print(is_rising(iterator))
+#
+# # TEST_3:
+# iterator = iter(list(range(100, 200)))
+#
+# print(is_rising(iterator))
+#
+# # TEST_4:
+# iterator = iter(list(range(200, 100, -1)))
+#
+# print(is_rising(iterator))
+#
+# # TEST_5:
+# iterator = iter(list(range(100, 201)) + [200])
+#
+# print(is_rising(iterator))
+#
+# # TEST_6:
+# iterator = iter([10]*50)
+#
+# print(is_rising(iterator))
