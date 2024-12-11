2cfbfb42605585c5422ae4b98b7d62da461f9773
Nigan <brodvey77@gmail.com>
2024-12-11 13:47:01 +0300

    10_10_16

--- Stepik_3_python/iter_tools_module.py
+++ Stepik_3_python/iter_tools_module.py
@@ -650,3 +650,46 @@ from itertools import filterfalse, takewhile, dropwhile
 # iterator = iter([11, 10, 19, 1, 12, 100, 1, 13, 107])
 #
 # print(max_pair(iterator))
+
+# from itertools import tee, chain
+#
+# def ncycles(iterable, times):
+#     return chain(*tee(iterable, times))
+#
+#
+#
+#
+# # INPUT DATA:
+#
+# # TEST_1:
+# print(*ncycles([1, 2, 3, 4], 3))
+#
+# # TEST_2:
+# iterator = iter('bee')
+#
+# print(*ncycles(iterator, 4))
+#
+# # TEST_3:
+# iterator = iter([1])
+#
+# print(*ncycles(iterator, 10))
+#
+# # TEST_4:
+# iterator = ncycles(iter('b'), 1)
+#
+# print(next(iterator))
+#
+# # TEST_5:
+# iterator = ncycles(iter('g'), 3)
+#
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+#
+# # TEST_6:
+# iterator = ncycles(iter([10, 10, 10, 10, 10]), 5)
+#
+# print(*iterator)
+#
+# # TEST_7:
+# print(list(ncycles([], 5)))
