7259ecb76fdcfffde7ea3c9331010b6306995dcb
Nigan <brodvey77@gmail.com>
2024-12-24 11:50:12 +0300

    10_11_12

--- Stepik_3_python/iter_tools_module.py
+++ Stepik_3_python/iter_tools_module.py
@@ -782,28 +782,45 @@ from itertools import filterfalse, takewhile, dropwhile
 
 
 
-from collections import namedtuple
-from itertools import groupby
-
-Student = namedtuple('Student', ['surname', 'name', 'grade'])
-
-students = [Student('Гагиев', 'Александр', 10), Student('Дедегкаев', 'Илья', 11), Student('Кодзаев', 'Георгий', 10),
-            Student('Набокова', 'Алиса', 11), Student('Кораев', 'Артур', 10), Student('Шилин', 'Александр', 11),
-            Student('Уртаева', 'Илина', 11), Student('Салбиев', 'Максим', 10), Student('Капустин', 'Илья', 11),
-            Student('Гудцев', 'Таймураз', 11), Student('Перчиков', 'Максим', 10), Student('Чен', 'Илья', 11),
-            Student('Елькина', 'Мария', 11),Student('Макоев', 'Руслан', 11), Student('Албегов', 'Хетаг', 11),
-            Student('Щербак', 'Илья', 10), Student('Идрисов', 'Баграт', 11), Student('Гапбаев', 'Герман', 10),
-            Student('Цивинская', 'Анна', 10), Student('Туткевич', 'Юрий', 11), Student('Мусиков', 'Андраник', 11),
-            Student('Гадзиев', 'Георгий', 11), Student('Белов', 'Юрий', 11), Student('Акоева', 'Диана', 11),
-            Student('Денисов', 'Илья', 11), Student('Букулова', 'Диана', 10), Student('Акоева', 'Лера', 11)]
-
-
-
-gr_st_name = groupby(sorted(students, key=lambda x: x.name), key=lambda x: x.name)
-
-
-l = []
-for k, data in gr_st_name:
-    l.append([k, len(list(i.name for i in data))])
-
-print(max(l, key=lambda x: x[1] )[0])
+# from collections import namedtuple
+# from itertools import groupby
+#
+# Student = namedtuple('Student', ['surname', 'name', 'grade'])
+#
+# students = [Student('Гагиев', 'Александр', 10), Student('Дедегкаев', 'Илья', 11), Student('Кодзаев', 'Георгий', 10),
+#             Student('Набокова', 'Алиса', 11), Student('Кораев', 'Артур', 10), Student('Шилин', 'Александр', 11),
+#             Student('Уртаева', 'Илина', 11), Student('Салбиев', 'Максим', 10), Student('Капустин', 'Илья', 11),
+#             Student('Гудцев', 'Таймураз', 11), Student('Перчиков', 'Максим', 10), Student('Чен', 'Илья', 11),
+#             Student('Елькина', 'Мария', 11),Student('Макоев', 'Руслан', 11), Student('Албегов', 'Хетаг', 11),
+#             Student('Щербак', 'Илья', 10), Student('Идрисов', 'Баграт', 11), Student('Гапбаев', 'Герман', 10),
+#             Student('Цивинская', 'Анна', 10), Student('Туткевич', 'Юрий', 11), Student('Мусиков', 'Андраник', 11),
+#             Student('Гадзиев', 'Георгий', 11), Student('Белов', 'Юрий', 11), Student('Акоева', 'Диана', 11),
+#             Student('Денисов', 'Илья', 11), Student('Букулова', 'Диана', 10), Student('Акоева', 'Лера', 11)]
+#
+#
+#
+# gr_st_name = groupby(sorted(students, key=lambda x: x.name), key=lambda x: x.name)
+#
+#
+# l = []
+# for k, data in gr_st_name:
+#     l.append([k, len(list(i.name for i in data))])
+#
+# print(max(l, key=lambda x: x[1] )[0])
+#
+# from itertools import groupby
+#
+# s = sorted(input().split(), key=len)
+#
+# iter_group = groupby(s, key=len)
+#
+#
+# for k, v in iter_group:
+#     print(f'{k} -> {", ".join(sorted(v))}')
+#
+#
+#
+# from itertools import groupby
+#
+# for k, v in groupby(sorted(input().split(), key = len), key = len):
+#     print(f'{k} -> {", ".join(sorted(v))}')
