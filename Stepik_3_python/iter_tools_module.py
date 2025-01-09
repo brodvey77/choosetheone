767dfa512cc1c673aaba686c1ceb5db9148573cc
Nigan <brodvey77@gmail.com>
2025-01-09 13:14:32 +0300

    10_12_12

--- Stepik_3_python/iter_tools_module.py
+++ Stepik_3_python/iter_tools_module.py
@@ -911,6 +911,16 @@ from itertools import filterfalse, takewhile, dropwhile
 # for i in s:
 #     print(*i, sep='')
 
-
-
+#
+# from itertools import combinations
+#
+# wallet = [100, 100, 50, 50, 50, 50, 20, 20, 20, 10, 10, 10, 10, 10, 5, 5, 1, 1, 1, 1, 1]
+#
+# counter = 0
+# for i in range(1, len(wallet)):
+#     for j in set(combinations(wallet, i)):
+#         if sum(j) == 100:
+#             counter += 1
+#
+# print(counter)
 
