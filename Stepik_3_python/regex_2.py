3b656b6e989de9d8206b18985efc9de2d5e14fef
Nigan <brodvey77@gmail.com>
2025-01-15 14:55:15 +0300

    11_6_11

--- Stepik_3_python/regex_2.py
+++ Stepik_3_python/regex_2.py
@@ -16,3 +16,22 @@
 #     match1 = fullmatch(pattern, login.strip())
 #     if match1:
 #         print(True)
+
+# import sys
+# from re import search, match, fullmatch
+#
+# pattern = r'^(\w+)\1$'
+#
+# for word in sys.stdin:
+#     match1 = fullmatch(pattern, word.strip('\n'))
+#     if match1:
+#         print(f'{match1.group()}')
+#
+#
+#
+# import sys
+# import re
+#
+# for line in map(str.rstrip, sys.stdin):
+#     if re.fullmatch(r'(\w+)\1', line):
+#         print(line)
