ff2003af6be078b2e2f6cfb67bec54a5fdf88239
Nigan <brodvey77@gmail.com>
2025-01-15 14:28:41 +0300

    11_6_10

--- Stepik_3_python/regex_2.py
+++ Stepik_3_python/regex_2.py
@@ -6,3 +6,13 @@
 #     match1 = fullmatch(pattern, i.strip())
 #     print(f'Код страны: {match1.group("country")}, Код города: {match1.group("city")}, Номер: {match1.group("number")}')
 
+
+# import sys
+# from re import search, fullmatch
+#
+# pattern = r'_\d+[A-Za-z]*_?$'
+#
+# for login in sys.stdin:
+#     match1 = fullmatch(pattern, login.strip())
+#     if match1:
+#         print(True)
