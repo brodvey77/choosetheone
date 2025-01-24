fbc4b358318d8de38efe399ec2da5c9c5d53632e
Zorin_Kolomna <s264157250r@gmail.com>
2025-01-24 20:48:15 +0300

    11_8_9

--- Stepik_3_python/regex_2.py
+++ Stepik_3_python/regex_2.py
@@ -236,3 +236,74 @@ import re
 #
 #
 
+# from re import sub
+
+# result = sub(r'\w+', lambda m: m.group().capitalize(), 'ThiS WORLD doesNT need A HeRo')
+
+# print(result)
+
+# from re import sub
+
+# def convert(match_obj):
+#     year, month, day = match_obj.group().split('-')
+#     return f'{day}.{month}.{year}'
+
+# result = sub(r'(\d{4}-\d{2}-\d{2})', convert, 'Gvido was born on 1956-01-31')
+
+# print(result)
+
+import re
+
+
+def normalize_jpeg(filename):
+    s = re.sub(r'(jpe?g)$', 'jpg', filename, flags=re.I)
+    return s
+
+
+
+# INPUT DATA:
+
+# TEST_1:
+print(normalize_jpeg('stepik.jPeG'))
+
+# TEST_2:
+print(normalize_jpeg('mountains.JPG'))
+
+# TEST_3:
+print(normalize_jpeg('windows11.jpg'))
+
+# TEST_4:
+print(normalize_jpeg('jepg_file.jPG'))
+
+# TEST_5:
+print(normalize_jpeg('file_jepg.jPeG'))
+
+# TEST_6:
+print(normalize_jpeg('file.jepg.JPEG'))
+
+# TEST_7:
+print(normalize_jpeg('filename.jpg.jpg'))
+
+# TEST_8:
+print(normalize_jpeg('stepik.jpeg.jpeg'))
+
+# TEST_9:
+print(normalize_jpeg('stepik.jpg.jpeg'))
+
+# TEST_10:
+print(normalize_jpeg('stepik.jpeg.jpg'))
+
+# TEST_11:
+print(normalize_jpeg('beegeek.JPg'))
+
+# TEST_12:
+print(normalize_jpeg('нарусском.JPg'))
+
+# TEST_13:
+print(normalize_jpeg('на русском языке.JPG'))
+
+# TEST_14:
+print(normalize_jpeg('jpg.jPg.Jpg.JPG'))
+
+# TEST_15:
+print(normalize_jpeg('Это тест.JpEg'))
