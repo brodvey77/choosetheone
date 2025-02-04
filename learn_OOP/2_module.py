df9d7e6c1446759a00a54c57c14aa7972f5dd4ba
choosetheone <brodvey77@yandex.ru>
2025-02-04 10:27:47 +0300

    2_1_5

--- learn_OOP/2_module.py
+++ learn_OOP/2_module.py
@@ -1,7 +1,19 @@
 
-n = int(input())
+# n = int(input())
 
-for i in range(n):
-    for j in range(n):
-        print(min(i + 1, j + 1, n - i, n - j), end=' ')
-    print()
+# for i in range(n):
+#     for j in range(n):
+#         print(min(i + 1, j + 1, n - i, n - j), end=' ')
+#     print()
+
+
+
+def is_correct_bracket_sequence(sequence):
+    count = 0
+    for char in sequence:
+        count = count + 1 if char == '(' else count - 1
+        if count < 0:
+            return False
+    return count == 0
+
+print(is_correct_bracket_sequence(input()))
