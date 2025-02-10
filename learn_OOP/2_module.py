b439c257e6dd909c476943bf05fdbb4afc6bd45d
Nigan <brodvey77@gmail.com>
2025-02-10 16:13:48 +0300

    2_1_14

--- learn_OOP/2_module.py
+++ learn_OOP/2_module.py
@@ -441,3 +441,187 @@
 #             x = datetime.date(day=i[3], month=mounth, year=year)
 #
 # print(x.strftime('%d.%m.%Y'))
+
+
+# def is_decimal(string):
+#     try:
+#         float(string)
+#         return True
+#     except ValueError:
+#         return False
+#
+#
+#
+# # INPUT DATA:
+#
+# # TEST_1:
+# print(is_decimal('100'))
+#
+# # TEST_2:
+# print(is_decimal('199.1'))
+#
+# # TEST_3:
+# print(is_decimal('-0.2'))
+#
+# # TEST_4:
+# print(is_decimal('.-95'))
+#
+# # TEST_5:
+# print(is_decimal('-.95'))
+#
+# # TEST_6:
+# print(is_decimal('.10'))
+#
+# # TEST_7:
+# print(is_decimal('.'))
+#
+# # TEST_8:
+# print(is_decimal(''))
+#
+# # TEST_9:
+# print(is_decimal('10..1'))
+#
+# # TEST_10:
+# print(is_decimal('--10.1'))
+#
+# # TEST_11:
+# print(is_decimal('---1.1'))
+#
+# # TEST_12:
+# print(is_decimal('1-.1'))
+#
+# # TEST_13:
+# print(is_decimal('1.-1'))
+#
+# # TEST_14:
+# print(is_decimal('1.1-'))
+#
+# # TEST_15:
+# print(is_decimal('1.1.1'))
+#
+# # TEST_16:
+# print(is_decimal('a.a'))
+#
+# # TEST_17:
+# print(is_decimal('a'))
+#
+# # TEST_18:
+# print(is_decimal('348209348203758348635465'))
+#
+# # TEST_19:
+# print(is_decimal('3482093.48203758348635465'))
+#
+# # TEST_20:
+# print(is_decimal('1-1'))
+#
+# # TEST_21:
+# print(is_decimal('1-1-1'))
+#
+# # TEST_22:
+# strings = ['100.', '349..', '-425.', '-1248..']
+# for string in strings:
+#     print(is_decimal(string))
+#
+# # TEST_23:
+# print(is_decimal('-0001'))
+# print(is_decimal('0001'))
+#
+# # TEST_24:
+# print(is_decimal('0.99.'))
+# print(is_decimal('-0.99.'))
+# print(is_decimal('.99.'))
+# print(is_decimal('-.99.'))
+
+# import re
+#
+# def is_fraction(string: str) -> bool:
+#     if re.fullmatch(r'-?\d+/0*[1-9]\d*', string):
+#         return True
+#     else:
+#         return False
+#
+#
+#
+#
+# # INPUT DATA:
+#
+# # TEST_1:
+# print(is_fraction('1000/1'))
+#
+# # TEST_2:
+# print(is_fraction('-54/9'))
+#
+# # TEST_3:
+# print(is_fraction('71'))
+#
+# # TEST_4:
+# print(is_fraction('1/0'))
+#
+# # TEST_5:
+# print(is_fraction(''))
+#
+# # TEST_6:
+# print(is_fraction('/4'))
+#
+# # TEST_7:
+# print(is_fraction('1000'))
+#
+# # TEST_8:
+# print(is_fraction('-987/1'))
+#
+# # TEST_9:
+# print(is_fraction('0/1'))
+#
+# # TEST_10:
+# print(is_fraction('-/56'))
+#
+# # TEST_11:
+# print(is_fraction('1/1234'))
+#
+# # TEST_12:
+# print(is_fraction('2-/4'))
+#
+# # TEST_13:
+# print(is_fraction('3/-7'))
+#
+# # TEST_14:
+# print(is_fraction('5/8-'))
+#
+# # TEST_15:
+# print(is_fraction('--1/2'))
+#
+# # TEST_16:
+# print(is_fraction('-7/3-'))
+#
+# # TEST_17:
+# print(is_fraction('-7-/-3-'))
+#
+# # TEST_18:
+# print(is_fraction('/4/5'))
+#
+# # TEST_19:
+# print(is_fraction('4/5/'))
+#
+# # TEST_20:
+# print(is_fraction('54365486548645/472342935648904709456'))
+#
+# # TEST_21:
+# print(is_fraction('5/2/4'))
+#
+# # TEST_22:
+# print(is_fraction('5/2/4/2'))
+#
+# # TEST_23:
+# print(is_fraction('1000/10'))
+#
+# # TEST_24:
+# print(is_fraction('1000/00001'))
+# print(is_fraction('-1000/00001'))
+#
+# # TEST_25:
+# print(is_fraction('1000/00004123'))
+# print(is_fraction('1000/0000'))
+# print(is_fraction('1000/00000008000'))
+
+
+
