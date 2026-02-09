a520879b71204752a292289a6633acaef23eceb5
Nigan <brodvey77@gmail.com>
2026-02-09 13:16:05 +0300

    4/3/7

--- Stepik_Python_OOP/draft.py
+++ Stepik_Python_OOP/draft.py
@@ -76,33 +76,51 @@
 # house.add_rooms(1)
 
 # print(house.color)
-# print(house.rooms)
+# # print(house.rooms)
+#
+# class Bee:
+#     def __init__(self, x=0, y=0) -> None:
+#         self.x = x
+#         self.y = y
+#
+#     def move_up(self, n):
+#         self.y += n
+#
+#     def move_down(self, n):
+#         self.y -= n
+#
+#     def move_right(self, n):
+#         self.x += n
+#
+#     def move_left(self, n):
+#         self.x -= n
+#
+#
+#
+# bee = Bee()
+#
+# bee.move_right(2)
+# bee.move_right(2)
+# bee.move_up(3)
+# bee.move_left(1)
+# bee.move_down(1)
+#
+# print(bee.x, bee.y)
 
-class Bee:
-    def __init__(self, x=0, y=0) -> None:
-        self.x = x
-        self.y = y
 
-    def move_up(self, n):
-        self.y += n
-
-    def move_down(self, n):
-        self.y -= n
-
-    def move_right(self, n):
-        self.x += n
-
-    def move_left(self, n):
-        self.x -= n
-
-
-
-bee = Bee()
-
-bee.move_right(2)
-bee.move_right(2)
-bee.move_up(3)
-bee.move_left(1)
-bee.move_down(1)
-
-print(bee.x, bee.y)
+# from itertools import cycle
+#
+# class Gun():
+#     def __init__(self):
+#         self.sounds = cycle(['pif', 'paf'])
+#
+#     def shoot(self):
+#         print(next(self.sounds))
+#
+#
+# gun = Gun()
+#
+# gun.shoot()
+# gun.shoot()
+# gun.shoot()
+# gun.shoot()
