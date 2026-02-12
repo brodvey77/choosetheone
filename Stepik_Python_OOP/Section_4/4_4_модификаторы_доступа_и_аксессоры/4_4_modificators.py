3f0001deccfa057fbbb549ef7f47df4a211dbb53
Nigan <brodvey77@gmail.com>
2026-02-12 15:59:37 +0300

    add 4_4 info

--- Stepik_Python_OOP/Section_4/4_4_модификаторы_доступа_и_аксессоры/4_4_modificators.py
+++ Stepik_Python_OOP/Section_4/4_4_модификаторы_доступа_и_аксессоры/4_4_modificators.py
@@ -1,11 +1,64 @@
-class ElectricCar:
-    def __init__(self, color):
-        self.__color = color
+# class ElectricCar:
+#     def __init__(self, color):
+#         self.__color = color
+#
+#     def get_color(self):
+#         return self.__color
+#
+#
+# car = ElectricCar('black')
+#
+# car._ElectricCar__color = 'yellow'
+#
+# print(car.__dict__)
+# print(car.get_color())
+
+#
+# class Cat:
+#     def __init__(self, name):
+#         self._name = name
+#
+#     def get_name(self):
+#         return self._name
+#
+#     def set_name(self, name):
+#         if isinstance(name, str) and name.isalpha():
+#             self._name = name
+#         else:
+#             raise ValueError('Не корректное имя')
+#
+#
+#
+# cat = Cat('Кемаль')
+# print(cat.get_name())
+#
+# cat.set_name('dод3жер')
+# print(cat.get_name())
 
 
-car = ElectricCar('black')
+# class Cat:
+#     def __init__(self, name):
+#         self._name = name
+#
+#     def get_name(self):
+#         return self._name
+#
+#     def set_name(self, name):
+#         if isinstance(name, str) and name.isalpha():
+#             self._name = name
+#         else:
+#             raise ValueError('Некорректное имя')
+#
+#     def del_name(self):
+#         del self._name
+#
+#
+# cat = Cat('Кемаль')
+#
+# print(cat.get_name())
+# print(Cat.__dict__)
+#
+# cat.del_name()
+# print(cat.__dict__)
+# print(cat.get_name())
 
-car._ElectricCar__color = 'yellow'
-
-print(car.__dict__)
-print(car.__color)
