4a6c12054c646c71b0b522a64452ac65811c7d8f
Nigan <brodvey77@gmail.com>
2026-02-16 13:20:31 +0300

    add 4_4_17

---
+++ Stepik_Python_OOP/Section_4/4_4_модификаторы_доступа_и_аксессоры/4_4_17.py
@@ -0,0 +1,23 @@
+class User:
+    def __init__(self, name, age):
+        self.set_name(name)
+        self.set_age(age)
+
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, new_name):
+        if isinstance(new_name, str) and new_name.isalpha() and len(new_name) != 0:
+            self._name = new_name
+        else:
+            raise ValueError('Некорректное имя')
+
+    def get_age(self):
+        return self._age
+
+    def set_age(self, new_age):
+        if isinstance(new_age, int) and 0 <= new_age <= 110:
+            self._age = new_age
+        else:
+            raise ValueError('Некорректный возраст')
