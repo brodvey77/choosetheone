132e179357977ee72dcfc132872cb50d166a191e
Nigan <brodvey77@gmail.com>
2026-03-02 15:26:21 +0300

    add 4_7_11

--- Stepik_Python_OOP/draft.py
+++ Stepik_Python_OOP/draft.py
@@ -489,3 +489,18 @@ from numpy.ma.core import argsort, append
 #     def fullname(self, fullname):
 #         self.name, self.surname = fullname.split()
 
+# class ElectricCar:
+#     status = True
+#
+#     def disable(self):
+#         self.__class__.status = False
+#
+#
+# car1 = ElectricCar()
+# car2 = ElectricCar()
+#
+# print(car1.status, car2.status)
+#
+# car1.disable()
+#
+# print(car1.status, car2.status)
