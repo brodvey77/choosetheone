72495e4f5d258bb23fb92dcecd408349e1e6d2fc
Nigan <brodvey77@gmail.com>
2026-03-03 15:13:31 +0300

    done 4_7_14

---
+++ Stepik_Python_OOP/Section_4/4_7_decorators @classmethod and @staticmethod/4_7_14.py
@@ -0,0 +1,32 @@
+class Pet:
+    pets = []
+    def __init__(self, name):
+        self.name = name
+        Pet.pets.append(self)
+
+    @classmethod
+    def first_pet(cls):
+        if len(cls.pets)> 0:
+            return cls.pets[0]
+
+    @classmethod
+    def last_pet(cls):
+        if len(cls.pets) > 0:
+            return cls.pets[-1]
+
+    @classmethod
+    def num_of_pets(cls):
+        return len(cls.pets)
+
+
+pet1 = Pet('Ratchet')
+pet2 = Pet('Clank')
+pet3 = Pet('Rivet')
+
+print(Pet.first_pet().name)
+print(Pet.last_pet().name)
+print(Pet.num_of_pets())
+
+
+
+
