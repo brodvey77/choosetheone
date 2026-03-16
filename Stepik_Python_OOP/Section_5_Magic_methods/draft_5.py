dad095599c91446bd2593c3d17ac4414085728fa
Nigan <brodvey77@gmail.com>
2026-03-16 16:24:49 +0300

    add info 5_2

--- Stepik_Python_OOP/Section_5_Magic_methods/draft_5.py
+++ Stepik_Python_OOP/Section_5_Magic_methods/draft_5.py
@@ -38,3 +38,14 @@
 # car = ElectricCar('yellow')
 #
 # print(car.color)
+
+class ElectricCar:
+    def __del__(self):
+        print('Вызов метода __del__()')
+
+
+cars = [ElectricCar(), ElectricCar()]
+
+del cars
+
+print('Завершение работы')
