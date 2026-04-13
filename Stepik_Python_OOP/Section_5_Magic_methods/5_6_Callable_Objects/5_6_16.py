0437714d1e8daacdd4f0c30f4616e50aab2019c8
Nigan <brodvey77@gmail.com>
2026-04-13 13:58:47 +0300

    done 5_6_16

---
+++ Stepik_Python_OOP/Section_5_Magic_methods/5_6_Callable_Objects/5_6_16.py
@@ -0,0 +1,37 @@
+
+class CountCalls:
+    calls = 0
+    def __init__(self, func):
+        self.func = func
+
+
+    def __call__(self, *args, **kwargs):
+        self.calls += 1
+        return self.func(*args, *kwargs)
+
+
+@CountCalls
+def square(a):
+    return a ** 2
+
+
+for i in range(100):
+    square(i)
+
+print(square.calls)
+
+
+class CountCalls:
+    def __init__(self, func):
+        self.func = func
+        self.calls = 0
+
+    def __call__(self, *args, **kwargs):
+        self.calls += 1
+        return self.func(*args, **kwargs)
+
+
+
+
+
+
