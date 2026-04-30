4b4f11f8d03a83ed491cc3428f481051a81ff9ef
Nigan <brodvey77@gmail.com>
2026-04-30 13:36:47 +0300

    done 5_10_13

--- Stepik_Python_OOP/Section_5_Magic_methods/5_10_Object_Hashing_Part_2/5_10_13.py
+++ Stepik_Python_OOP/Section_5_Magic_methods/5_10_Object_Hashing_Part_2/5_10_13.py
@@ -36,3 +36,37 @@ try:
     point.color = 'black'
 except AttributeError as e:
     print('Error')
+
+
+class ColoredPoint:
+    def __init__(self, x, y, color):
+        self._x = x
+        self._y = y
+        self._color = color
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+    @property
+    def color(self):
+        return self._color
+
+    @property
+    def _fields(self):
+        return self._x, self._y, self._color
+
+    def __repr__(self):
+        return "ColoredPoint({}, {}, '{}')".format(*self._fields)
+
+    def __eq__(self, other):
+        if isinstance(other, ColoredPoint):
+            return self._fields == other._fields
+        return NotImplemented
+
+    def __hash__(self):
+        return hash(self._fields)
