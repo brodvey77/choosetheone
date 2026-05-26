20b05237cee3bd07364abe6252a342e591f597da
Nigan <brodvey77@gmail.com>
2026-05-26 13:27:03 +0300

    done 6_2_16_ai

--- Stepik_Python_OOP/Section_6_Protocols/6_2_Sequence_Protocol/6_2_16.py
+++ Stepik_Python_OOP/Section_6_Protocols/6_2_Sequence_Protocol/6_2_16.py
@@ -104,3 +104,24 @@ class Grouper:
 
     def __getitem__(self, key):
         return self._groups[key]
+
+
+
+
+from collections import UserDict, defaultdict
+
+class Grouper(UserDict):
+    def __init__(self, iterable, key):
+        self.key = key
+        self.data = defaultdict(list)
+        for i in iterable:
+            self.data[key(i)].append(i)
+
+    def __iter__(self):
+        yield from self.data.items()
+
+    def add(self, obj):
+        self.data[self.key(obj)].append(obj)
+
+    def group_for(self, obj):
+        return self.key(obj)
