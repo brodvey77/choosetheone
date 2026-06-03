6bb8978f4d1ec0f3ea504a9bb16cf42fc0b1f2cb
Nigan <brodvey77@gmail.com>
2026-06-03 10:24:39 +0300

    done 6_5_5

---
+++ Stepik_Python_OOP/Section_6_Protocols/6_5_The_Context_Manager_Protocol_Part_2/6_5_5.py
@@ -0,0 +1,19 @@
+class Greeter:
+    def __init__(self, name):
+        self.name = name
+
+    def __enter__(self):
+        print(f"Приветствую, {self.name}!")
+        return self
+
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print(f"До встречи, {self.name}!")
+        return False
+
+
+
+
+
+with Greeter('Кейв') as greeter:
+    print(greeter.name)
