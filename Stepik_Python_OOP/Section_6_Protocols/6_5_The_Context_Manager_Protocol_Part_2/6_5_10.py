64ae18512ceb3d7e3e274c27839c963077259078
Win_11 <s264157250r@gmail.com>
2026-07-02 17:22:11 +0300

    add new info

---
+++ Stepik_Python_OOP/Section_6_Protocols/6_5_The_Context_Manager_Protocol_Part_2/6_5_10.py
@@ -0,0 +1,33 @@
+class Suppress:
+    def __init__(self, *exceptions):
+        self.exceptions = exceptions
+        self.exception = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type is not None and issubclass(exc_type, self.exceptions):
+            self.exception = exc_value
+            return True
+        return False
+
+
+
+
+with Suppress(NameError):
+    print('Этой переменной не существует -->', variable)
+
+print('Завершение программы')
+
+with Suppress(TypeError, ValueError) as context:
+    number = int('я число')
+
+print(context.exception)
+print(type(context.exception))
+
+
+with Suppress() as context:
+    print('All success!')
+
+print(context.exception)
