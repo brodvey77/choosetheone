1c7741b80fc4e442e1922b2b9e2ab4ab41fb587e
Win_11 <s264157250r@gmail.com>
2026-07-02 17:22:53 +0300

    add new info

--- Stepik_Python_OOP/Section_6_Protocols/6_5_The_Context_Manager_Protocol_Part_2/6_5_10.py
+++ Stepik_Python_OOP/Section_6_Protocols/6_5_The_Context_Manager_Protocol_Part_2/6_5_10.py
@@ -13,6 +13,21 @@ class Suppress:
         return False
 
 
+class Suppress:
+    def __init__(self, *exceptions):
+        self.exceptions = exceptions
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type in self.exceptions:
+            self.exception = exc_val
+            return True
+        self.exception = None
+        return False
+
+
 
 
 with Suppress(NameError):
