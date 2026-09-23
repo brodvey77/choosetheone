875f816048fefa849f61dce9c1518351e65b8908
Nigan <brodvey77@gmail.com>
2026-09-23 15:38:55 +0300

    done 7_2_19 and _20

--- Stepik_Python_OOP/Section_7_Inheritance_and_polymorphism/7_2_Inheritance_Part_2/7_2_20.py
+++ Stepik_Python_OOP/Section_7_Inheritance_and_polymorphism/7_2_Inheritance_Part_2/7_2_20.py
@@ -1,58 +1,83 @@
-from functools import reduce
+# from functools import reduce
+#
+# class Summator:
+#     def total(self, n):
+#         return reduce(lambda x,y: x+y, range(1, n+1))
+#
+# class SquareSummator(Summator):
+#     def total(self, n):
+#         return n * (n + 1) * (2 * n + 1) // 6
+#
+# class QubeSummator(Summator):
+#     def total(self, n):
+#         return int((n * (n + 1) / 2) ** 2)
+#
+# class CustomSummator(Summator):
+#     def __init__(self, m):
+#         self.m = m
+#     def total(self, n):
+#         return sum(i**self.m for i in range(1, n+1))
+#
+#
+#
+#
+# for i in range(5, 50):
+#     summator = CustomSummator(i)
+#     print(summator.total(10))
+#
+#
+#
+#
+#
+# class Summator:
+#     def transform(self, n):
+#         return n
+#
+#     def total(self, n):
+#         return sum(self.transform(i) for i in range(1, n + 1))
+#
+#
+# class SquareSummator(Summator):
+#     def transform(self, n):
+#         return n ** 2
+#
+#
+# class QubeSummator(Summator):
+#     def transform(self, n):
+#         return n ** 3
+#
+#
+# class CustomSummator(Summator):
+#     def __init__(self, power):
+#         self.power = power
+#
+#     def transform(self, n):
+#         return n ** self.power
+#
+#
+# class Summator:
+#     def __init__(self, m=1):
+#         self.m = m
+#
+#     def total(self, n):
+#         return sum(map(lambda x: x ** self.m, range(1, n + 1)))
+#
+#
+# class SquareSummator(Summator):
+#     def __init__(self):
+#         super().__init__(2)
+#
+#
+# class QubeSummator(Summator):
+#     def __init__(self):
+#         super().__init__(3)
+#
+#
+# class CustomSummator(Summator):
+#     def __init__(self, m):
+#         super().__init__(m)
 
-class Summator:
-    def total(self, n):
-        return reduce(lambda x,y: x+y, range(1, n+1))
 
-class SquareSummator(Summator):
-    def total(self, n):
-        return n * (n + 1) * (2 * n + 1) // 6
-
-class QubeSummator(Summator):
-    def total(self, n):
-        return int((n * (n + 1) / 2) ** 2)
-
-class CustomSummator(Summator):
-    def __init__(self, m):
-        self.m = m
-    def total(self, n):
-        return sum(i**self.m for i in range(1, n+1))
-
-
-
-
-for i in range(5, 50):
-    summator = CustomSummator(i)
-    print(summator.total(10))
-
-
-
-
-
-class Summator:
-    def transform(self, n):
-        return n
-
-    def total(self, n):
-        return sum(self.transform(i) for i in range(1, n + 1))
-
-
-class SquareSummator(Summator):
-    def transform(self, n):
-        return n ** 2
-
-
-class QubeSummator(Summator):
-    def transform(self, n):
-        return n ** 3
-
-
-class CustomSummator(Summator):
-    def __init__(self, power):
-        self.power = power
-
-    def transform(self, n):
-        return n ** self.power
 
 
 class Summator:
@@ -60,8 +85,7 @@ class Summator:
         self.m = m
 
     def total(self, n):
-        return sum(map(lambda x: x ** self.m, range(1, n + 1)))
-
+        return sum(map(lambda x: x**self.m, range(1, n+1)))
 
 class SquareSummator(Summator):
     def __init__(self):
@@ -72,7 +96,20 @@ class QubeSummator(Summator):
     def __init__(self):
         super().__init__(3)
 
-
 class CustomSummator(Summator):
     def __init__(self, m):
         super().__init__(m)
+
+
+
+
+
+
+
+summator1 = Summator()
+summator2 = CustomSummator(2)
+summator3 = CustomSummator(3)
+
+print(summator1.total(3))    # 1 + 2 + 3
+print(summator2.total(3))    # 1 + 4 + 9
+print(summator3.total(3))    # 1 + 8 + 27
